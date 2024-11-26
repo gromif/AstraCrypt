8d865c69e6ddfcfe0face575ef67c57785ca9bf7
Nevidimka655 <alex.kobrys@gmail.com>
2024-11-27 01:16:01 +0200

    Remove old code

--- app/src/main/java/com/nevidimka655/astracrypt/MainActivity.kt
+++ app/src/main/java/com/nevidimka655/astracrypt/MainActivity.kt
@@ -5,23 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.google.android.material.color.DynamicColors
-import com.nevidimka655.astracrypt.databinding.MainBinding
 import com.nevidimka655.astracrypt.ui.Main
 import com.nevidimka655.astracrypt.utils.Engine
 import com.nevidimka655.astracrypt.utils.appearance.AppearanceManager
-import com.nevidimka655.astracrypt.utils.extensions.lazyFast
 import com.nevidimka655.crypto.tink.TinkConfig
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    private val binding by lazyFast { MainBinding.inflate(layoutInflater) }
-    private val appBarConfigurationMainIds = setOf(
-        R.id.homeFragment, R.id.filesFragment, R.id.starredFragment, R.id.settingsFragment,
-        R.id.authFragment
-    )
-    val fab get() = binding.fab
-    val fabLarge get() = binding.fabLarge
 
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
@@ -30,31 +21,11 @@ class MainActivity : ComponentActivity() {
         setupDynamicColors()
         TinkConfig.init()
         setContent { Main() }
-        /*setContentView(binding.root)
-        setupToolbar()
-        setupSnackbarsFlow()
-        savedInstanceState ?: shouldShowAuthScreen(navController)
+        /*savedInstanceState ?: shouldShowAuthScreen(navController)
         navController.addOnDestinationChangedListener { _, dest, _ ->
             setUiStateOnFragment(dest.id)
         }
-        registerAppBarOffsetListener()
-        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
-            val bars = insets.getInsets(
-                WindowInsetsCompat.Type.systemBars() or WindowInsetsCompat.Type.displayCutout()
-            )
-            v.updatePadding(
-                left = bars.left,
-                top = bars.top,
-                right = bars.right
-            )
-            binding.bottomNavigationView.updatePadding(bottom = bars.bottom)
-            WindowInsetsCompat.CONSUMED
-        }
         with(binding) {
-            bottomNavigationView.run {
-                setBackgroundColor(ColorManager.navigationBarColor)
-                setupWithNavController(navController)
-            }
             movePanel.setBackgroundColor(ColorManager.navigationBarColor)
             move.setOnClickListener {
                 vm.move(
@@ -65,7 +36,6 @@ class MainActivity : ComponentActivity() {
             }
             cancel.setOnClickListener { vm.selectorManager.closeActionMode() }
         }
-        vm.uiStateFlow.withLifecycle(lifecycleScope, lifecycle) { parseUiState(it) }
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             val isPermissionsGranted = ContextCompat.checkSelfPermission(
                 this,
@@ -108,36 +78,15 @@ class MainActivity : ComponentActivity() {
 
     /*@SuppressLint("PrivateResource")
     private fun setUiStateOnFragment(fragmentDest: Int) {
-        val isDestinationMain = appBarConfigurationMainIds.contains(fragmentDest)
-        var uiState = UiState(
-            navBarEnabled = isDestinationMain,
-            navBarColorTinted = isDestinationMain
-        )
-        if (isDestinationMain) vm.toolsManager.releaseMemory()
         uiState = when (fragmentDest) {
-
-            R.id.homeFragment, R.id.settingsFragment -> {
-                uiState
-            }
-
-            R.id.authFragment, R.id.calculatorFragment -> uiState.copy(
-                toolbarState = false,
-                toolbarStateAnimated = false,
-                navBarEnabled = false,
-                navBarColorTinted = false
-            )
 
             R.id.aeadFragment, R.id.notesViewFragment, R.id.archiveFragment -> {
                 fabLarge.setImageResource(R.drawable.ic_done)
-                uiState.copy(fabLargeState = true)
             }
 
             R.id.notesFragment -> {
                 fabLarge.setImageResource(com.google.android.material.R.drawable.material_ic_edit_black_24dp)
-                uiState.copy(fabLargeState = true)
             }
-
-            else -> uiState
         }
     }*/
 
