669c5fa664881cc9d1d5f1cf501b3623691c9362
Nevidimka655 <alex.kobrys@gmail.com>
2024-11-27 22:47:40 +0200

    Remove deprecated statusBarColor

--- app/src/main/java/com/nevidimka655/astracrypt/ui/theme/Theme.kt
+++ app/src/main/java/com/nevidimka655/astracrypt/ui/theme/Theme.kt
@@ -12,8 +12,6 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
@@ -115,7 +113,6 @@ fun AstraCryptTheme(
     val activity = (view.context as Activity)
     if (!view.isInEditMode) SideEffect {
         val window = activity.window
-        window.statusBarColor = Color.Transparent.toArgb()
         WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
     }
     MaterialTheme(
