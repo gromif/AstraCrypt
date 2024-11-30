a8f308b64634d0854a803f3ed46df968f4f0a07e
Nevidimka655 <alex.kobrys@gmail.com>
2024-11-30 17:10:07 +0200

    Add defaultDataStoreManager injection

--- app/src/main/java/com/nevidimka655/astracrypt/di/AuthModule.kt
+++ app/src/main/java/com/nevidimka655/astracrypt/di/AuthModule.kt
@@ -1,6 +1,7 @@
 package com.nevidimka655.astracrypt.di
 
 import com.nevidimka655.astracrypt.features.auth.AuthManager
+import com.nevidimka655.astracrypt.utils.datastore.DefaultDataStoreManager
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -13,6 +14,10 @@ class AuthModule {
 
     @Singleton
     @Provides
-    fun provideAuthManager() = AuthManager()
+    fun provideAuthManager(
+        defaultDataStoreManager: DefaultDataStoreManager
+    ) = AuthManager(
+        defaultDataStoreManager = defaultDataStoreManager
+    )
 
 }
