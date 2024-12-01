a76328c6708b8845e315dd2c23c42b92ab1afc67
Nevidimka655 <alex.kobrys@gmail.com>
2024-12-01 18:45:49 +0200

    Add Repository Hilt module

---
+++ app/src/main/java/com/nevidimka655/astracrypt/di/RepositoryModule.kt
@@ -0,0 +1,47 @@
+package com.nevidimka655.astracrypt.di
+
+import android.content.Context
+import androidx.room.Room
+import com.nevidimka655.astracrypt.room.AppDatabase
+import com.nevidimka655.astracrypt.room.Repository
+import com.nevidimka655.astracrypt.room.RepositoryEncryption
+import com.nevidimka655.astracrypt.room.daos.NotesDao
+import com.nevidimka655.astracrypt.room.daos.StorageItemDao
+import com.nevidimka655.crypto.tink.KeysetFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+private const val DATABASE_NAME = "storage"
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RepositoryModule {
+
+    @Singleton
+    @Provides
+    fun provideRepository(database: AppDatabase, storage: StorageItemDao, notes: NotesDao) =
+        Repository(database = database, storage = storage, notes = notes)
+
+    @Singleton
+    @Provides
+    fun provideRepositoryEncryption(keysetFactory: KeysetFactory) =
+        RepositoryEncryption(keysetFactory = keysetFactory)
+
+    @Singleton
+    @Provides
+    fun provideStorageDao(database: AppDatabase) = database.getStorageItemDao()
+
+    @Singleton
+    @Provides
+    fun provideNotesDao(database: AppDatabase) = database.getNotesDao()
+
+    @Singleton
+    @Provides
+    fun provideRoomDatabase(@ApplicationContext context: Context) =
+        Room.databaseBuilder(context, AppDatabase::class.java, DATABASE_NAME).build()
+
+}
