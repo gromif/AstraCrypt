0dfca150d8ce81255f24e4ab6942ec47c1a30068
Nevidimka655 <alex.kobrys@gmail.com>
2024-12-01 17:40:24 +0200

    Remove runtime KeysetFactory initialisation

--- app/src/main/java/com/nevidimka655/astracrypt/utils/Engine.kt
+++ app/src/main/java/com/nevidimka655/astracrypt/utils/Engine.kt
@@ -2,11 +2,7 @@ package com.nevidimka655.astracrypt.utils
 
 import android.content.Context
 import androidx.work.WorkManager
-import com.nevidimka655.astracrypt.utils.shared_prefs.PrefsKeys
-import com.nevidimka655.astracrypt.utils.shared_prefs.PrefsManager
-import com.nevidimka655.crypto.tink.KeysetFactory
 import com.nevidimka655.haptic.Haptic
-import java.io.File
 
 /**
  * Contains singleton dependencies.
@@ -24,17 +20,6 @@ object Engine {
         Haptic.init(context = context)
         if (!::appContext.isInitialized) {
             appContext = context
-        }
-        KeysetFactory.Config.run {
-            if (dataFileName.isEmpty()) {
-                dataFileName = "grapefruit.ss0"
-                dataFile = File("${Io(appContext, Randomizer()).dataDir}/$dataFileName")
-                dataLength = AppConfig.CRYPTO_NONCE_SIZE
-                dataPasswordHashLength = AppConfig.AUTH_PASSWORD_HASH_LENGTH
-                prefsFileNameDefault = PrefsKeys.FileNames.MASTER
-                prefs = PrefsManager.clear
-                prefsUniqueSaltFieldKey = PrefsKeys.ENCRYPTION_UNIQUE_SALT
-            }
         }
     }
 
