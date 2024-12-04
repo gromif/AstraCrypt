220f4f2131f94aedf7f540479f8c6f94d6b7a854
Nevidimka655 <alex.kobrys@gmail.com>
2024-12-04 13:32:50 +0200

    Annotate Api checks

--- app/src/main/java/com/nevidimka655/astracrypt/utils/Api.kt
+++ app/src/main/java/com/nevidimka655/astracrypt/utils/Api.kt
@@ -1,16 +1,31 @@
 package com.nevidimka655.astracrypt.utils
 
 import android.os.Build
+import androidx.annotation.ChecksSdkIntAtLeast
 
 object Api {
 
-    private val sdkInt get() = Build.VERSION.SDK_INT
+    private val sdkInt = Build.VERSION.SDK_INT
 
-    fun atLeastAndroid9() = sdkInt >= Build.VERSION_CODES.P
-    fun atLeastAndroid8() = sdkInt >= Build.VERSION_CODES.O
-    fun atLeastAndroid7() = sdkInt >= Build.VERSION_CODES.N
-    fun atLeastAndroid10() = sdkInt >= Build.VERSION_CODES.Q
-    fun atLeastAndroid11() = sdkInt >= Build.VERSION_CODES.R
-    fun atLeastAndroid13() = sdkInt >= Build.VERSION_CODES.TIRAMISU
+    @ChecksSdkIntAtLeast(api = Build.VERSION_CODES.O)
+    fun atLeast8() = sdkInt >= Build.VERSION_CODES.O
+
+    @ChecksSdkIntAtLeast(api = Build.VERSION_CODES.P)
+    fun atLeast9() = sdkInt >= Build.VERSION_CODES.P
+
+    @ChecksSdkIntAtLeast(api = Build.VERSION_CODES.N)
+    fun atLeast7() = sdkInt >= Build.VERSION_CODES.N
+
+    @ChecksSdkIntAtLeast(api = Build.VERSION_CODES.Q)
+    fun atLeast10() = sdkInt >= Build.VERSION_CODES.Q
+
+    @ChecksSdkIntAtLeast(api = Build.VERSION_CODES.R)
+    fun atLeast11() = sdkInt >= Build.VERSION_CODES.R
+
+    @ChecksSdkIntAtLeast(api = Build.VERSION_CODES.S)
+    fun atLeast12() = sdkInt >= Build.VERSION_CODES.S
+
+    @ChecksSdkIntAtLeast(api = Build.VERSION_CODES.TIRAMISU)
+    fun atLeast13() = sdkInt >= Build.VERSION_CODES.TIRAMISU
 
 }
