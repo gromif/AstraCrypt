1ca14553c1945f54ad24353d9cae7ac2f665dd79
Nevidimka655 <alex.kobrys@gmail.com>
2024-12-05 00:49:48 +0200

    Remove Material3 Views library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -136,7 +136,7 @@ dependencies {
     implementation(libs.media.exifinterface)
 
     // Google
-    implementation(libs.google.android.material)
+    //implementation(libs.google.android.material)
     implementation(libs.google.crypto.tink)
 
     // DI - Hilt
