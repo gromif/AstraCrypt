73e6d75d3d7123d7cd3a21370db50c1cf5450507
Nevidimka655 <alex.kobrys@gmail.com>
2024-12-04 13:43:47 +0200

    Remove unused code

--- app/src/main/java/com/nevidimka655/astracrypt/utils/EthernetTools.kt
+++ app/src/main/java/com/nevidimka655/astracrypt/utils/EthernetTools.kt
@@ -2,7 +2,6 @@ package com.nevidimka655.astracrypt.utils
 
 import android.content.Context
 import android.content.Intent
-import android.net.ConnectivityManager
 import android.net.Uri
 
 object EthernetTools {
@@ -13,9 +12,4 @@ object EthernetTools {
         context = context,
         url = "https://play.google.com/store/apps/details?id=com.nevidimka655.astracrypt"
     )
-
-    fun isConnected(context: Context = Engine.appContext): Boolean {
-        val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        return cm.activeNetwork != null && cm.getNetworkCapabilities(cm.activeNetwork) != null
-    }
 }
