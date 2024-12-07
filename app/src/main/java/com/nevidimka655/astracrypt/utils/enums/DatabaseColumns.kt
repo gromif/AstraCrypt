647f07245819b936ec8b6fefeb10772d1c1d2490
Nevidimka655 <alex.kobrys@gmail.com>
2024-12-07 23:00:36 +0200

    Update columns

--- app/src/main/java/com/nevidimka655/astracrypt/utils/enums/DatabaseColumns.kt
+++ app/src/main/java/com/nevidimka655/astracrypt/utils/enums/DatabaseColumns.kt
@@ -1,6 +1,18 @@
 package com.nevidimka655.astracrypt.utils.enums
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
 enum class DatabaseColumns {
-    Details, Name, Thumbnail, Path,
-    EncryptionType, ThumbEncryptionType, ID
+    @SerialName("a") ID,
+    @SerialName("b") Name,
+    @SerialName("c") ItemType,
+    @SerialName("d") Parent,
+    @SerialName("e") Size,
+    @SerialName("f") State,
+    @SerialName("g") Preview,
+    @SerialName("h") File,
+    @SerialName("i") Flags,
+    @SerialName("j") Time
 }
