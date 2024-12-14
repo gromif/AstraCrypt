87a646e979e715e172e5dfa505ed7c6864262710
Nevidimka655 <alex.kobrys@gmail.com>
2024-12-14 21:12:08 +0200

    Set InstallIn to ViewModel

--- app/src/main/java/com/nevidimka655/astracrypt/app/di/work/WorkManagerModule.kt
+++ app/src/main/java/com/nevidimka655/astracrypt/app/di/work/WorkManagerModule.kt
@@ -5,12 +5,12 @@ import androidx.work.WorkManager
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
-import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
-@InstallIn(SingletonComponent::class)
+@InstallIn(ViewModelComponent::class)
 object WorkManagerModule {
 
     @Singleton
