1ee85f009f2f1a8cc79c9774094c60f17c073a73
Nevidimka655 <alex.kobrys@gmail.com>
2024-12-14 16:01:13 +0200

    Replace test data with password hash

--- app/src/main/java/com/nevidimka655/astracrypt/data/datastore/DefaultDataStoreManager.kt
+++ app/src/main/java/com/nevidimka655/astracrypt/data/datastore/DefaultDataStoreManager.kt
@@ -4,17 +4,24 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
+import com.nevidimka655.crypto.tink.extensions.fromBase64
+import com.nevidimka655.crypto.tink.extensions.toBase64
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
 
 class DefaultDataStoreManager(
     private val dataStore: DataStore<Preferences>
 ) {
+    private val PASSWORD_HASH = stringPreferencesKey("papaya")
+    val passwordHashFlow = dataStore.data.map {
+        val hash = it[PASSWORD_HASH]
+        if (!hash.isNullOrEmpty()) hash.fromBase64()
+        else ByteArray(0)
+    }
+    suspend fun getPasswordHash() = passwordHashFlow.first()
 
-    private val PASSWORD_CHECK_TEST_DATA = stringPreferencesKey("papaya")
-    suspend fun getPasswordCheckTestData() = dataStore.data.first()[PASSWORD_CHECK_TEST_DATA] ?: ""
-
-    suspend fun setPasswordCheckTestDataFlow(value: String) = dataStore.edit { preferences ->
-        preferences[PASSWORD_CHECK_TEST_DATA] = value
+    suspend fun setPasswordHash(hash: ByteArray?) = dataStore.edit { preferences ->
+        preferences[PASSWORD_HASH] = hash?.toBase64() ?: ""
     }
 
 }
