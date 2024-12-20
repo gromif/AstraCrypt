2c81d2ead0a473ddfa1a57b83c4dd974e60c518c
Nevidimka655 <alex.kobrys@gmail.com>
2024-12-20 15:38:21 +0200

    Inject Base64Service

--- app/src/main/java/com/nevidimka655/astracrypt/data/datastore/SettingsDataStoreManager.kt
+++ app/src/main/java/com/nevidimka655/astracrypt/data/datastore/SettingsDataStoreManager.kt
@@ -9,13 +9,12 @@ import com.nevidimka655.astracrypt.data.model.AeadInfo
 import com.nevidimka655.astracrypt.domain.model.auth.Auth
 import com.nevidimka655.astracrypt.domain.model.profile.Avatars
 import com.nevidimka655.astracrypt.domain.model.profile.ProfileInfo
+import com.nevidimka655.crypto.tink.core.encoders.Base64Service
 import com.nevidimka655.crypto.tink.core.hash.Sha256Service
 import com.nevidimka655.crypto.tink.data.KeysetManager
 import com.nevidimka655.crypto.tink.domain.KeysetTemplates
 import com.nevidimka655.crypto.tink.extensions.aeadPrimitive
 import com.nevidimka655.crypto.tink.extensions.deterministicAeadPrimitive
-import com.nevidimka655.crypto.tink.extensions.fromBase64
-import com.nevidimka655.crypto.tink.extensions.toBase64
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 import kotlinx.serialization.encodeToString
@@ -27,7 +26,8 @@ private const val KEYSET_TAG_VALUE = "l0_ShH%OLq"
 class SettingsDataStoreManager(
     private val dataStore: DataStore<Preferences>,
     private val keysetManager: KeysetManager,
-    private val sha256Service: Sha256Service
+    private val sha256Service: Sha256Service,
+    private val base64Service: Base64Service
 ) {
     private val encryptionSettingsKey = intPreferencesKey("a1")
     suspend fun getEncryptionSettings() = dataStore.data.first()[encryptionSettingsKey] ?: 0
@@ -89,7 +89,8 @@ class SettingsDataStoreManager(
             keyParams = KeysetTemplates.DeterministicAEAD.AES256_SIV.params
         ).deterministicAeadPrimitive()
         val associatedData = sha256Service.compute(value = key.toByteArray())
-        aead.encryptDeterministically(key.toByteArray(), associatedData).toBase64()
+        val encryptedBytes = aead.encryptDeterministically(key.toByteArray(), associatedData)
+        base64Service.encode(bytes = encryptedBytes)
     } ?: key
 
     private suspend fun encryptValue(key: String, value: String) = getEncryptionTemplate()?.let {
@@ -97,7 +98,8 @@ class SettingsDataStoreManager(
             tag = KEYSET_TAG_VALUE,
             keyParams = it.params
         ).aeadPrimitive()
-        aead.encrypt(value.toByteArray(), key.toByteArray()).toBase64()
+        val encryptedBytes = aead.encrypt(value.toByteArray(), key.toByteArray())
+        base64Service.encode(bytes = encryptedBytes)
     } ?: value
 
     private suspend fun decryptValue(key: String, value: String) = getEncryptionTemplate()?.let {
@@ -105,6 +107,7 @@ class SettingsDataStoreManager(
             tag = KEYSET_TAG_VALUE,
             keyParams = it.params
         ).aeadPrimitive()
-        aead.decrypt(value.fromBase64(), key.toByteArray()).decodeToString()
+        val encryptedBytes = base64Service.decode(value = value)
+        aead.decrypt(encryptedBytes, key.toByteArray()).decodeToString()
     } ?: value
 }
