f503160f4060af83694a926ca272ab65c95041a7
Nevidimka655 <alex.kobrys@gmail.com>
2024-12-22 14:06:04 +0200

    Simplify build script

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -1,20 +1,11 @@
-import org.gradle.kotlin.dsl.withType
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
     `kotlin-dsl`
 }
 
 group = "com.nevidimka655.buildlogic"
-java {
-    sourceCompatibility = JavaVersion.VERSION_21
-    targetCompatibility = JavaVersion.VERSION_21
-}
-tasks.withType<KotlinCompile>().configureEach {
-    compilerOptions {
-        jvmTarget.set(JvmTarget.JVM_21)
-    }
+
+kotlin {
+    jvmToolchain(21)
 }
 
 dependencies {
