0e02209f7f7963138de64ec69600753214883615
Nevidimka655 <alex.kobrys@gmail.com>
2024-12-22 17:50:50 +0200

    Add VersionCatalog extension

--- build-logic/convention/src/main/java/com/nevidimka655/buildlogic/ProjectExtensions.kt
+++ build-logic/convention/src/main/java/com/nevidimka655/buildlogic/ProjectExtensions.kt
@@ -2,7 +2,13 @@ package com.nevidimka655.buildlogic
 
 import com.android.build.api.dsl.CommonExtension
 import org.gradle.api.Project
+import org.gradle.api.artifacts.VersionCatalog
+import org.gradle.api.artifacts.VersionCatalogsExtension
+import org.gradle.kotlin.dsl.getByType
 import org.jetbrains.kotlin.gradle.dsl.kotlinExtension
+
+val Project.libs
+    get(): VersionCatalog = extensions.getByType<VersionCatalogsExtension>().named("libs")
 
 internal fun Project.configureDefaultConfig(
     commonExtension: CommonExtension<*,*,*,*,*,*>
