582f0631edaa3e630875763d07266617d2f3e29b
Nevidimka655 <alex.kobrys@gmail.com>
2024-12-22 14:26:57 +0200

    Add Versions object

--- build-logic/convention/src/main/java/com/nevidimka655/buildlogic/AppConfig.kt
+++ build-logic/convention/src/main/java/com/nevidimka655/buildlogic/AppConfig.kt
@@ -12,4 +12,13 @@ internal object AppConfig {
         const val JVM_TOOLCHAIN_VERSION = 21
     }
 
+    object Versions {
+        private const val MAJOR = 1
+        private const val MINOR = 7
+        private const val PATCH = 3
+
+        const val CODE = MAJOR * 10000 + MINOR * 100 + PATCH
+        const val NAME = "$MAJOR.$MINOR.$PATCH"
+    }
+
 }
