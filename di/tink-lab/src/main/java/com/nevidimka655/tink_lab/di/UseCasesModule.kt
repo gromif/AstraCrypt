203dd4ec80c374cce1ded00835fae9ed1463a1da
Nevidimka655 <alex.kobrys@gmail.com>
2024-12-24 10:41:49 +0200

    Add getAeadList use cases

--- di/tink-lab/src/main/java/com/nevidimka655/tink_lab/di/UseCasesModule.kt
+++ di/tink-lab/src/main/java/com/nevidimka655/tink_lab/di/UseCasesModule.kt
@@ -3,7 +3,10 @@ package com.nevidimka655.tink_lab.di
 import com.nevidimka655.crypto.tink.core.encoders.HexService
 import com.nevidimka655.crypto.tink.core.hash.Sha256Service
 import com.nevidimka655.crypto.tink.data.serializers.SerializeKeysetByKeyService
+import com.nevidimka655.tink_lab.domain.model.Repository
 import com.nevidimka655.tink_lab.domain.usecase.CreateLabKeyUseCase
+import com.nevidimka655.tink_lab.domain.usecase.GetFileAeadListUseCase
+import com.nevidimka655.tink_lab.domain.usecase.GetTextAeadListUseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -23,5 +26,13 @@ object UseCasesModule {
         sha256Service = sha256Service,
         hexService = hexService
     )
+
+    @Provides
+    fun provideGetFileAeadListUseCase(repository: Repository) =
+        GetFileAeadListUseCase(repository = repository)
+
+    @Provides
+    fun provideGetTextAeadListUseCase(repository: Repository) =
+        GetTextAeadListUseCase(repository = repository)
 
 }
