dc64802511103812c00c146ee9406ec96015769f
Nevidimka655 <alex.kobrys@gmail.com>
2024-12-24 09:35:02 +0200

    Add Fab callback

--- app/src/main/java/com/nevidimka655/astracrypt/view/composables/lab/tink/Navigation.kt
+++ app/src/main/java/com/nevidimka655/astracrypt/view/composables/lab/tink/Navigation.kt
@@ -1,5 +1,8 @@
 package com.nevidimka655.astracrypt.view.composables.lab.tink
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.runtime.remember
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import com.nevidimka655.astracrypt.resources.R
@@ -7,18 +10,26 @@ import com.nevidimka655.astracrypt.view.models.UiState
 import com.nevidimka655.astracrypt.view.navigation.Route
 import com.nevidimka655.tink_lab.TinkLabKeyScreen
 import com.nevidimka655.ui.compose_core.wrappers.TextWrap
+import kotlinx.coroutines.channels.Channel
 
 @Suppress("ObjectPropertyName")
 val _LabTinkUiState = UiState(
     toolbar = UiState.Toolbar(
         title = TextWrap.Resource(id = R.string.lab_aeadEncryption)
-    )
+    ),
+    fab = UiState.Fab(icon = Icons.Default.Done)
 )
 
 fun NavGraphBuilder.tinkKey(
     onUiStateChange: (UiState) -> Unit
 ) = composable<Route.LabGraph.TinkGraph.Key> {
     onUiStateChange(_LabTinkUiState)
+    val onRequestKeysetChannel = remember { Channel<Unit>() }
 
-    TinkLabKeyScreen()
+    TinkLabKeyScreen(
+        onRequestKeysetChannel = onRequestKeysetChannel,
+        onFinish = {
+
+        }
+    )
 }
