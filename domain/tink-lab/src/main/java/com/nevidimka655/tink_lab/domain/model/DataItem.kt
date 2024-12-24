5ed64dd91e44bfb472ee7fc2bb5b0acada8e3528
Nevidimka655 <alex.kobrys@gmail.com>
2024-12-24 10:54:35 +0200

    Remove android-specific dependency

--- domain/tink-lab/src/main/java/com/nevidimka655/tink_lab/domain/model/DataItem.kt
+++ domain/tink-lab/src/main/java/com/nevidimka655/tink_lab/domain/model/DataItem.kt
@@ -1,13 +1,11 @@
 package com.nevidimka655.tink_lab.domain.model
 
-import androidx.annotation.StringRes
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class DataItem(
     @SerialName("a")
-    @StringRes
     val titleResId: Int,
 
     @SerialName("b")
