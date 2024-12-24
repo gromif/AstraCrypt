68c9c0c6d363392048c0448700e717fc822eb8c8
Nevidimka655 <alex.kobrys@gmail.com>
2024-12-24 11:57:55 +0200

    Remove serialization annotations

--- domain/tink-lab/src/main/java/com/nevidimka655/tink_lab/domain/model/DataItem.kt
+++ domain/tink-lab/src/main/java/com/nevidimka655/tink_lab/domain/model/DataItem.kt
@@ -1,13 +1,6 @@
 package com.nevidimka655.tink_lab.domain.model
 
-import kotlinx.serialization.SerialName
-import kotlinx.serialization.Serializable
-
-@Serializable
 data class DataItem(
-    @SerialName("a")
     val titleResId: Int,
-
-    @SerialName("b")
     val type: DataType
 )
