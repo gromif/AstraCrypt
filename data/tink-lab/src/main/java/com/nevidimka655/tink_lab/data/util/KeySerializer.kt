59314eb61cb1659ac52007027ad9d24a79a870fd
Nevidimka655 <alex.kobrys@gmail.com>
2024-12-24 13:10:26 +0200

    Add KeySerializer

---
+++ data/tink-lab/src/main/java/com/nevidimka655/tink_lab/data/util/KeySerializer.kt
@@ -0,0 +1,16 @@
+package com.nevidimka655.tink_lab.data.util
+
+import com.nevidimka655.astracrypt.utils.Serializer
+import com.nevidimka655.crypto.tink.core.encoders.HexService
+import com.nevidimka655.tink_lab.data.dto.KeyDto
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+
+class KeySerializer(
+    private val hexService: HexService
+): Serializer<KeyDto, String> {
+    override fun invoke(item: KeyDto): String {
+        val keyDtoJson = Json.encodeToString<KeyDto>(item)
+        return hexService.encode(keyDtoJson.toByteArray())
+    }
+}
