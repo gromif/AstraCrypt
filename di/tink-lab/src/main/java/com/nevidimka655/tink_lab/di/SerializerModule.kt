44126744103b6ab337c07b2fc1279cbe328770ad
Nevidimka655 <alex.kobrys@gmail.com>
2024-12-24 13:39:50 +0200

    Add KeySerializer DI

---
+++ di/tink-lab/src/main/java/com/nevidimka655/tink_lab/di/SerializerModule.kt
@@ -0,0 +1,20 @@
+package com.nevidimka655.tink_lab.di
+
+import com.nevidimka655.astracrypt.utils.Serializer
+import com.nevidimka655.crypto.tink.core.encoders.HexService
+import com.nevidimka655.tink_lab.data.dto.KeyDto
+import com.nevidimka655.tink_lab.data.util.KeySerializer
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+
+@Module
+@InstallIn(ViewModelComponent::class)
+object SerializerModule {
+
+    @Provides
+    fun provideKeySerializer(hexService: HexService): Serializer<KeyDto, String> =
+        KeySerializer(hexService = hexService)
+
+}
