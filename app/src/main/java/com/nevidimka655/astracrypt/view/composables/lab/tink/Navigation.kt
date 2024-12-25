9639fa140739c4ed33d9ee134beebab3a5923571
Nevidimka655 <alex.kobrys@gmail.com>
2024-12-26 00:52:07 +0200

    Return keyset to the outer layer

--- app/src/main/java/com/nevidimka655/astracrypt/view/composables/lab/tink/Navigation.kt
+++ app/src/main/java/com/nevidimka655/astracrypt/view/composables/lab/tink/Navigation.kt
@@ -39,7 +39,7 @@ fun NavGraphBuilder.tinkKey(
 
     TinkLabKeyScreen(
         onRequestKeysetChannel = onRequestKeysetChannel.receiveAsFlow(),
-        onFinish = {
+        navigateToTextMode = {
 
         }
     )
