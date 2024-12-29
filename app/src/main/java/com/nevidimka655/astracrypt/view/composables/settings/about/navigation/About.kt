7489792c377fbee5d562f63d3305bb8f0658e713
Nevidimka655 <alex.kobrys@gmail.com>
2024-12-29 20:51:12 +0200

    Make ScreenState property private

--- app/src/main/java/com/nevidimka655/astracrypt/view/composables/settings/about/navigation/About.kt
+++ app/src/main/java/com/nevidimka655/astracrypt/view/composables/settings/about/navigation/About.kt
@@ -8,7 +8,7 @@ import com.nevidimka655.astracrypt.view.models.UiState
 import com.nevidimka655.astracrypt.view.navigation.Route
 import com.nevidimka655.ui.compose_core.wrappers.TextWrap
 
-val AboutUiState = UiState(
+private val AboutUiState = UiState(
     toolbar = UiState.Toolbar(
         title = TextWrap.Resource(id = R.string.settings_about)
     )
