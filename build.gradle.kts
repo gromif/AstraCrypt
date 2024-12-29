c631efb5bf45da276c2087a2d68e204f45b60fe9
Nevidimka655 <alex.kobrys@gmail.com>
2024-12-29 14:24:17 +0200

    Remove kotlin.navigation.safeargs views plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,5 @@ plugins {
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.hilt) apply false
-    alias(libs.plugins.kotlin.navigation.safeargs) apply false
     alias(libs.plugins.compose.compiler) apply false
 }
