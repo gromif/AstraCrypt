7cacb6ecec4bb2a49cbdaddba8837d1c5535e3fe
Nevidimka655 <alex.kobrys@gmail.com>
2024-12-30 20:13:13 +0200

    Change return type to File

--- core/utils/src/main/java/com/nevidimka655/astracrypt/utils/io/WorkerSerializer.kt
+++ core/utils/src/main/java/com/nevidimka655/astracrypt/utils/io/WorkerSerializer.kt
@@ -1,14 +1,17 @@
 package com.nevidimka655.astracrypt.utils.io
 
+import java.io.File
+import kotlin.collections.joinToString
+
 class WorkerSerializer(
     private val filesUtil: FilesUtil
 ) {
 
-    fun saveStringArrayToFile(arr: Array<String>) {
+    fun saveStringListToFile(arr: List<String>): File {
         val file = filesUtil.createTempFileInCache()
         val text = arr.joinToString(separator = "\n").trimEnd()
         file.writeText(text)
-        file.toString()
+        return file
     }
 
 }
