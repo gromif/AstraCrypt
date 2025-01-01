c88a76079e89a9f59a4eff3ccfe96ef2c46dd7f1
Nevidimka655 <alex.kobrys@gmail.com>
2025-01-01 11:02:13 +0200

    Add Hilt plugin

--- features/auth/login/build.gradle.kts
+++ features/auth/login/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.astracrypt.android.library)
     alias(libs.plugins.astracrypt.android.library.compose)
+    alias(libs.plugins.astracrypt.android.hilt)
     alias(libs.plugins.astracrypt.kotlin.coroutines)
 }
 
