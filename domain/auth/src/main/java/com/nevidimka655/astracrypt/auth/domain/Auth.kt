7aa806a13190ad2c5b4f3be9c5437ec738c3bda9
Nevidimka655 <alex.kobrys@gmail.com>
2025-01-02 01:42:54 +0200

    Add default constructor

--- domain/auth/src/main/java/com/nevidimka655/astracrypt/auth/domain/Auth.kt
+++ domain/auth/src/main/java/com/nevidimka655/astracrypt/auth/domain/Auth.kt
@@ -1,8 +1,8 @@
 package com.nevidimka655.astracrypt.auth.domain
 
 data class Auth(
-    val type: AuthType?,
-    val skin: Skin?,
-    val hintState: Boolean,
-    val hintText: String?
+    val type: AuthType? = null,
+    val skin: Skin? = null,
+    val hintState: Boolean = false,
+    val hintText: String? = null
 )
