0ea93679de5aea8129f9834dbe9ccd366c0ead39
gromif <alex.kobrys@gmail.com>
2025-01-05 18:49:00 +0200

    Cleanup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,14 +13,8 @@ plugins {
     alias(libs.plugins.astracrypt.android.work)
 }
 
-// Create a variable called keystorePropertiesFile, and initialize it to your
-// keystore.properties file, in the rootProject folder.
 val keystorePropertiesFile = rootProject.file("keystore.properties")
-
-// Initialize a new Properties() object called keystoreProperties.
 val keystoreProperties = Properties()
-
-// Load your keystore.properties file into the keystoreProperties object.
 keystoreProperties.load(FileInputStream(keystorePropertiesFile))
 
 android {
