6ffb1a0dc4c1c44c74271ecc370f2dff2c84958f
gromif <alex.kobrys@gmail.com>
2025-01-08 18:25:41 +0200

    Remove old config

--- app/src/main/java/com/nevidimka655/astracrypt/app/AppConfig.kt
+++ app/src/main/java/com/nevidimka655/astracrypt/app/AppConfig.kt
@@ -7,16 +7,10 @@ object AppConfig {
     const val DB_THUMB_QUALITY = 70
     const val DB_THUMB_SIZE = 500
 
-    const val CRYPTO_NONCE_SIZE = 64
-
     const val ITEM_NAME_MAX_SIZE = 90
 
     const val PROFILE_NAME_MAX_SIZE = 30
     const val PROFILE_IMAGE_SIZE = 300
-
-    const val AUTH_PASSWORD_HASH_LENGTH = 32
-    const val AUTH_PASSWORD_MAX_LENGTH = AUTH_PASSWORD_HASH_LENGTH
-    const val AUTH_HINT_MAX_LENGTH = 96
 
     const val PAGING_PAGE_SIZE = 30
     const val PAGING_INITIAL_LOAD = PAGING_PAGE_SIZE
