4aeea89e057619f0c4ea03603f8d9645c9765b59
gromif <alex.kobrys@gmail.com>
2025-01-09 23:35:37 +0200

    Add Screen

---
+++ features/settings/aead/src/main/java/io/gromif/astracrypt/settings/aead/Screen.kt
@@ -0,0 +1,39 @@
+package io.gromif.astracrypt.settings.aead
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.nevidimka655.ui.compose_core.PreferencesScreen
+import io.gromif.astracrypt.settings.aead.components.DatabaseGroup
+import io.gromif.astracrypt.settings.aead.components.FilesGroup
+import io.gromif.astracrypt.settings.aead.components.SettingsGroup
+
+@Preview(showBackground = true)
+@Composable
+internal fun Screen(
+    aeadTemplatesList: List<String> = listOf(),
+    aeadLargeStreamTemplateList: List<String> = listOf(),
+    aeadSmallStreamTemplateList: List<String> = listOf(),
+    notesAeadName: String? = "Test Notes Aead",
+    onNotesAeadChange: (Int) -> Unit = {},
+    filesAeadName: String? = "Test Files SAead",
+    onFilesAeadChange: (Int) -> Unit = {},
+    previewAeadName: String? = "Test Preview SAead",
+    onPreviewAeadChange: (Int) -> Unit = {}
+) {
+    PreferencesScreen {
+        FilesGroup(
+            aeadLargeStreamTemplateList = aeadLargeStreamTemplateList,
+            aeadSmallStreamTemplateList = aeadSmallStreamTemplateList,
+            filesAeadName = filesAeadName,
+            onFilesAeadChange = onFilesAeadChange,
+            previewAeadName = previewAeadName,
+            onPreviewAeadChange = onPreviewAeadChange
+        )
+        DatabaseGroup(
+            aeadTemplatesList = aeadTemplatesList,
+            notesAeadName = notesAeadName,
+            onNotesAeadChange = onNotesAeadChange
+        )
+        SettingsGroup()
+    }
+}
