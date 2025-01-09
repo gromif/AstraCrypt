56c9ac31ef168c93db43aee58ce58aa6b8c1fbe1
gromif <alex.kobrys@gmail.com>
2025-01-09 17:38:07 +0200

    Inject Repository

---
+++ di/settings/aead/src/main/java/io/gromif/astracrypt/settings/aead/di/RepositoryModule.kt
@@ -0,0 +1,24 @@
+package io.gromif.astracrypt.settings.aead.di
+
+import com.nevidimka655.domain.notes.repository.SettingsRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+import io.gromif.astracrypt.settings.aead.data.repository.RepositoryImpl
+import io.gromif.astracrypt.settings.aead.domain.repository.Repository
+
+@Module
+@InstallIn(ViewModelComponent::class)
+internal object RepositoryModule {
+
+    @Provides
+    fun provideRepository(
+        notesSettingsRepository: NotesSettingsRepository
+    ): Repository = RepositoryImpl(
+        notesSettingsRepository = notesSettingsRepository
+    )
+
+}
+
+typealias NotesSettingsRepository = SettingsRepository
