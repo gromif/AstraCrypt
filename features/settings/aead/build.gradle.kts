19bb3d03275f1abeb419fa15b30d29fa8e0fdd96
gromif <alex.kobrys@gmail.com>
2025-01-09 17:38:40 +0200

    Add hilt plugin

--- features/settings/aead/build.gradle.kts
+++ features/settings/aead/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     alias(libs.plugins.astracrypt.android.library)
+    alias(libs.plugins.astracrypt.android.hilt)
+    alias(libs.plugins.astracrypt.android.hilt.compose)
 }
 
 android {
