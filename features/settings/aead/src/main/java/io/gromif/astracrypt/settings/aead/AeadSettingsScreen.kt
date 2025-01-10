abc8eedc77801cf9a4f9f872180558e4db1e4083
gromif <alex.kobrys@gmail.com>
2025-01-10 10:24:44 +0200

    Add `without encryption` option

--- features/settings/aead/src/main/java/io/gromif/astracrypt/settings/aead/AeadSettingsScreen.kt
+++ features/settings/aead/src/main/java/io/gromif/astracrypt/settings/aead/AeadSettingsScreen.kt
@@ -3,16 +3,26 @@ package io.gromif.astracrypt.settings.aead
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.nevidimka655.astracrypt.resources.R
 
 @Composable
 fun AeadSettingsScreen() {
     val vm: AeadViewModel = hiltViewModel()
+    val noEncryptionOptionText = stringResource(R.string.withoutEncryption)
+    val baseOptionsList = remember { listOf(noEncryptionOptionText) }
 
-    val aeadTemplatesList = remember { vm.aeadTemplateList.map { it.name } }
-    val aeadLargeStreamTemplateList = remember { vm.aeadLargeStreamTemplateList.map { it.name } }
-    val aeadSmallStreamTemplateList = remember { vm.aeadSmallStreamTemplateList.map { it.name } }
+    val aeadTemplatesList = remember {
+        baseOptionsList + vm.aeadTemplateList.map { it.name }
+    }
+    val aeadLargeStreamTemplateList = remember {
+        baseOptionsList + vm.aeadLargeStreamTemplateList.map { it.name }
+    }
+    val aeadSmallStreamTemplateList = remember {
+        baseOptionsList + vm.aeadSmallStreamTemplateList.map { it.name }
+    }
 
     val notesAeadNameState by vm.notesAeadNameState.collectAsStateWithLifecycle()
 
