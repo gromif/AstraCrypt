675ea257d6dd30567c876c8a48551d94719805d2
gromif <alex.kobrys@gmail.com>
2025-01-13 22:19:13 +0200

    Add view mode flow usecase

---
+++ domain/files/src/main/java/io/gromif/astracrypt/files/domain/usecase/GetListViewModeUseCase.kt
@@ -0,0 +1,15 @@
+package io.gromif.astracrypt.files.domain.usecase
+
+import io.gromif.astracrypt.files.domain.model.ViewMode
+import io.gromif.astracrypt.files.domain.repository.SettingsRepository
+import kotlinx.coroutines.flow.Flow
+
+class GetListViewModeUseCase(
+    private val settingsRepository: SettingsRepository
+) {
+
+    operator fun invoke(): Flow<ViewMode> {
+        return settingsRepository.getViewModeFlow()
+    }
+
+}
