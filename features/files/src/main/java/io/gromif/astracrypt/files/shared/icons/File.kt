67d2055e2b5489c0dc0890685c0a1574b1d3b771
gromif <alex.kobrys@gmail.com>
2025-01-14 13:05:34 +0200

    Add file type icons

---
+++ features/files/src/main/java/io/gromif/astracrypt/files/shared/icons/File.kt
@@ -0,0 +1,77 @@
+package io.gromif.astracrypt.files.shared.icons
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.path
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.util.fastForEach
+import com.nevidimka655.atracrypt.core.design_system.darkScheme
+import com.nevidimka655.atracrypt.core.design_system.extendedColorScheme
+import com.nevidimka655.atracrypt.core.design_system.lightScheme
+import io.gromif.astracrypt.files.shared._FileType
+
+internal val _FileType.File @Composable get() = default()
+internal val _FileType.FileAlt @Composable get() = alt()
+
+@Composable
+private fun default(): ImageVector = with(MaterialTheme.extendedColorScheme.blue) {
+    file(background = colorContainer, foreground = onColorContainer)
+}
+
+@Composable
+private fun alt(): ImageVector = with(MaterialTheme.colorScheme) {
+    file(background = onSurfaceVariant, foreground = surface)
+}
+
+@Preview
+@Composable
+private fun Preview(vectors: List<ImageVector> = listOf(default(), alt())) = Column {
+    vectors.forEach { vector ->
+        Row {
+            listOf(lightScheme, darkScheme).fastForEach {
+                MaterialTheme(colorScheme = it) {
+                    Surface { Icon(vector, null, Modifier.size(128.dp), Color.Unspecified) }
+                }
+            }
+        }
+    }
+}
+
+private fun file(background: Color, foreground: Color) = ImageVector.Builder(
+    name = "file",
+    defaultWidth = 24.dp,
+    defaultHeight = 24.dp,
+    viewportWidth = 24f,
+    viewportHeight = 24f
+).apply {
+    // Background Rectangle Path
+    path(fill = SolidColor(background)) {
+        moveTo(14f, 2f)
+        horizontalLineTo(6f)
+        curveTo(4.9f, 2f, 4f, 2.9f, 4f, 4f)
+        verticalLineTo(20f)
+        curveTo(4f, 21.1f, 4.9f, 22f, 6f, 22f)
+        horizontalLineTo(18f)
+        curveTo(19.1f, 22f, 20f, 21.1f, 20f, 20f)
+        verticalLineTo(8f)
+        lineTo(14f, 2f)
+        close()
+    }
+    // Foreground Shape Path
+    path(fill = SolidColor(foreground)) {
+        moveTo(13f, 8f)
+        verticalLineTo(3.5f)
+        lineTo(17.5f, 8f)
+        close()
+    }
+}.build()
---
+++ features/files/src/main/java/io/gromif/astracrypt/files/shared/icons/Video.kt
@@ -0,0 +1,80 @@
+package io.gromif.astracrypt.files.shared.icons
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.path
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.util.fastForEach
+import com.nevidimka655.atracrypt.core.design_system.darkScheme
+import com.nevidimka655.atracrypt.core.design_system.extendedColorScheme
+import com.nevidimka655.atracrypt.core.design_system.lightScheme
+import io.gromif.astracrypt.files.shared._FileType
+
+internal val _FileType.Video @Composable get() = default()
+internal val _FileType.VideoAlt @Composable get() = alt()
+
+@Composable
+private fun default(): ImageVector = with(MaterialTheme.extendedColorScheme.violet) {
+    video(foreground = onColorContainer)
+}
+
+@Composable
+private fun alt(): ImageVector = with(MaterialTheme.colorScheme) {
+    video(foreground = onSurfaceVariant)
+}
+
+@Preview
+@Composable
+private fun Preview(vectors: List<ImageVector> = listOf(default(), alt())) = Column {
+    vectors.forEach { vector ->
+        Row {
+            listOf(lightScheme, darkScheme).fastForEach {
+                MaterialTheme(colorScheme = it) {
+                    Surface { Icon(vector, null, Modifier.size(128.dp), Color.Unspecified) }
+                }
+            }
+        }
+    }
+}
+
+private fun video(foreground: Color) = ImageVector.Builder(
+    name = "video",
+    defaultWidth = 24.dp,
+    defaultHeight = 24.dp,
+    viewportWidth = 24f,
+    viewportHeight = 24f
+).apply {
+    path(fill = SolidColor(foreground)) {
+        moveTo(18f, 4f)
+        lineTo(20f, 8f)
+        lineTo(17f, 8f)
+        lineTo(15f, 4f)
+        lineTo(13f, 4f)
+        lineTo(15f, 8f)
+        lineTo(12f, 8f)
+        lineTo(10f, 4f)
+        lineTo(8f, 4f)
+        lineTo(10f, 8f)
+        lineTo(7f, 8f)
+        lineTo(5f, 4f)
+        lineTo(4f, 4f)
+        curveTo(2.9f, 4f, 2.01f, 4.9f, 2.01f, 6f)
+        lineTo(2f, 18f)
+        curveTo(2f, 19.1f, 2.9f, 20f, 4f, 20f)
+        horizontalLineTo(20f)
+        curveTo(21.1f, 20f, 22f, 19.1f, 22f, 18f)
+        verticalLineTo(4f)
+        horizontalLineTo(18f)
+        close()
+    }
+}.build()
