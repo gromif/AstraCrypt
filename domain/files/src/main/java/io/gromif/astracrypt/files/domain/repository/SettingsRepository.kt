86d308887acad2415f3ec41d2b55074af5c3d916
gromif <alex.kobrys@gmail.com>
2025-01-21 18:08:15 +0200

    Reorganize methods

--- domain/files/src/main/java/io/gromif/astracrypt/files/domain/repository/SettingsRepository.kt
+++ domain/files/src/main/java/io/gromif/astracrypt/files/domain/repository/SettingsRepository.kt
@@ -1,19 +1,20 @@
 package io.gromif.astracrypt.files.domain.repository
 
+import io.gromif.astracrypt.files.domain.model.AeadInfo
 import io.gromif.astracrypt.files.domain.model.ViewMode
 import kotlinx.coroutines.flow.Flow
 
 interface SettingsRepository {
 
-    fun getAeadNameFlow(): Flow<String?>
-
-    suspend fun getAeadTemplateIndex(): Int
-
-    suspend fun setAeadTemplateIndex(aead: Int)
-
-
     fun getViewModeFlow(): Flow<ViewMode>
 
     suspend fun setViewMode(viewMode: ViewMode)
+
+
+    fun getAeadInfoFlow(): Flow<AeadInfo>
+
+    suspend fun getAeadInfo(): AeadInfo
+
+    suspend fun setAeadInfo(aeadInfo: AeadInfo)
 
 }
