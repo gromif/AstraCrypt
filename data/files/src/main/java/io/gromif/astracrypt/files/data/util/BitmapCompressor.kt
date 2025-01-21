6736f2a9fb35cbee7b854bb03bd9a4a896ad760c
gromif <alex.kobrys@gmail.com>
2025-01-21 18:09:26 +0200

    Add BitmapCompressor

---
+++ data/files/src/main/java/io/gromif/astracrypt/files/data/util/BitmapCompressor.kt
@@ -0,0 +1,17 @@
+package io.gromif.astracrypt.files.data.util
+
+import android.graphics.Bitmap
+import java.io.ByteArrayOutputStream
+
+class BitmapCompressor(
+    private val compressFormat: Bitmap.CompressFormat
+) {
+
+    operator fun invoke(bitmap: Bitmap, quality: Int): ByteArray {
+        val compressedByteStream = ByteArrayOutputStream().also {
+            bitmap.compress(compressFormat, quality, it)
+        }
+        return compressedByteStream.toByteArray()
+    }
+
+}
