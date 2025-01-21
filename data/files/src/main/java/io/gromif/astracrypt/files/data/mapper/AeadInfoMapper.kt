0d06d67478ec60ae2b95d0bca8039694cd247979
gromif <alex.kobrys@gmail.com>
2025-01-21 18:05:05 +0200

    Combine mappers

--- data/files/src/main/java/io/gromif/astracrypt/files/data/mapper/AeadInfoMapper.kt
+++ data/files/src/main/java/io/gromif/astracrypt/files/data/mapper/AeadInfoMapper.kt
@@ -4,16 +4,31 @@ import com.nevidimka655.astracrypt.utils.Mapper
 import io.gromif.astracrypt.files.data.dto.AeadInfoDto
 import io.gromif.astracrypt.files.domain.model.AeadInfo
 
+class AeadInfoDtoMapper: Mapper<AeadInfo, AeadInfoDto> {
+    override fun invoke(item: AeadInfo): AeadInfoDto {
+        return AeadInfoDto(
+            fileAeadIndex = item.fileAeadIndex,
+            previewAeadIndex = item.previewAeadIndex,
+            databaseAeadIndex = item.databaseAeadIndex,
+            isNameColumnEncrypted = item.name,
+            isPreviewColumnEncrypted = item.preview,
+            isPathColumnEncrypted = item.file,
+            isFlagColumnEncrypted = item.flag
+        )
+    }
+}
+
 class AeadInfoMapper: Mapper<AeadInfoDto, AeadInfo> {
     override fun invoke(item: AeadInfoDto): AeadInfo {
         return AeadInfo(
             fileAeadIndex = item.fileAeadIndex,
             previewAeadIndex = item.previewAeadIndex,
             databaseAeadIndex = item.databaseAeadIndex,
-            isNameColumnEncrypted = item.isNameColumnEncrypted,
-            isPreviewColumnEncrypted = item.isPreviewColumnEncrypted,
-            isPathColumnEncrypted = item.isPathColumnEncrypted,
-            isFlagColumnEncrypted = item.isFlagColumnEncrypted
+            db = item.databaseAeadIndex != -1,
+            name = item.isNameColumnEncrypted,
+            preview = item.isPreviewColumnEncrypted,
+            file = item.isPathColumnEncrypted,
+            flag = item.isFlagColumnEncrypted
         )
     }
 }
