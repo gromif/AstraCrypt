3614f49ee71dd4fd741a70f4c398c3b3725c950f
gromif <alex.kobrys@gmail.com>
2025-01-21 18:26:37 +0200

    Implement SettingsRepository

--- data/files/src/main/java/io/gromif/astracrypt/files/data/repository/SettingsRepositoryImpl.kt
+++ data/files/src/main/java/io/gromif/astracrypt/files/data/repository/SettingsRepositoryImpl.kt
@@ -4,28 +4,64 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.intPreferencesKey
-import com.nevidimka655.crypto.tink.domain.KeysetTemplates
+import androidx.datastore.preferences.core.stringPreferencesKey
+import com.nevidimka655.astracrypt.utils.Mapper
+import io.gromif.astracrypt.files.data.dto.AeadInfoDto
+import io.gromif.astracrypt.files.domain.model.AeadInfo
+import io.gromif.astracrypt.files.domain.model.ViewMode
 import io.gromif.astracrypt.files.domain.repository.SettingsRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 
 class SettingsRepositoryImpl(
-    private val dataStore: DataStore<Preferences>
+    private val dataStore: DataStore<Preferences>,
+    private val aeadInfoMapper: Mapper<AeadInfoDto, AeadInfo>,
+    private val aeadInfoDtoMapper: Mapper<AeadInfo, AeadInfoDto>
 ): SettingsRepository {
-    private val aeadKey = intPreferencesKey("aead")
-    override fun getAeadNameFlow(): Flow<String?> {
+    private val viewModeKey = intPreferencesKey("view_mode")
+    override fun getViewModeFlow(): Flow<ViewMode> {
         return dataStore.data.map {
-            val ordinal = it[aeadKey] ?: -1
-            KeysetTemplates.AEAD.entries.getOrNull(ordinal)?.name
+            val savedViewModeIndex = it[viewModeKey] ?: 0
+            ViewMode.entries[savedViewModeIndex]
         }
     }
 
-    override suspend fun getAeadTemplateIndex(): Int {
-        return dataStore.data.map { it[aeadKey] ?: -1 }.first()
+    override suspend fun setViewMode(viewMode: ViewMode) {
+        dataStore.edit { preferences ->
+            preferences[viewModeKey] = viewMode.ordinal
+        }
     }
 
-    override suspend fun setAeadTemplateIndex(aead: Int) {
-        dataStore.edit { it[aeadKey] = aead }
+
+    private val aeadInfoKey = stringPreferencesKey("aead_info")
+    private var cachedAeadInfo: AeadInfo? = null
+    override fun getAeadInfoFlow(): Flow<AeadInfo> {
+        return dataStore.data.map {
+            val cached = cachedAeadInfo
+            if (cached != null) cached else {
+                val serializedValue = it[aeadInfoKey]
+                val aeadInfo = if (serializedValue != null) {
+                    val dto: AeadInfoDto = Json.decodeFromString(serializedValue)
+                    aeadInfoMapper(dto)
+                } else AeadInfo()
+                cachedAeadInfo = aeadInfo
+                aeadInfo
+            }
+        }
+    }
+
+    override suspend fun getAeadInfo(): AeadInfo {
+        return cachedAeadInfo ?: getAeadInfoFlow().first()
+    }
+
+    override suspend fun setAeadInfo(aeadInfo: AeadInfo) {
+        cachedAeadInfo = aeadInfo
+        val dto: AeadInfoDto = aeadInfoDtoMapper(aeadInfo)
+        dataStore.edit { preferences ->
+            preferences[aeadInfoKey] = Json.encodeToString(dto)
+        }
     }
 }
---
+++ di/files/src/main/java/io/gromif/astracrypt/files/di/SettingsRepositoryModule.kt
@@ -0,0 +1,43 @@
+package io.gromif.astracrypt.files.di
+
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import com.nevidimka655.astracrypt.utils.Mapper
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import io.gromif.astracrypt.files.data.dto.AeadInfoDto
+import io.gromif.astracrypt.files.data.mapper.AeadInfoDtoMapper
+import io.gromif.astracrypt.files.data.mapper.AeadInfoMapper
+import io.gromif.astracrypt.files.data.repository.SettingsRepositoryImpl
+import io.gromif.astracrypt.files.domain.model.AeadInfo
+import io.gromif.astracrypt.files.domain.repository.SettingsRepository
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal object SettingsRepositoryModule {
+
+    @Singleton
+    @Provides
+    fun provideSettingsRepository(
+        @FilesDataStore
+        dataStore: DataStore<Preferences>,
+        aeadInfoMapper: Mapper<AeadInfoDto, AeadInfo>,
+        aeadInfoDtoMapper: Mapper<AeadInfo, AeadInfoDto>
+    ): SettingsRepository = SettingsRepositoryImpl(
+        dataStore = dataStore,
+        aeadInfoMapper = aeadInfoMapper,
+        aeadInfoDtoMapper = aeadInfoDtoMapper
+    )
+
+    @Singleton
+    @Provides
+    fun provideAeadInfoDtoMapper(): Mapper<AeadInfo, AeadInfoDto> = AeadInfoDtoMapper()
+
+    @Singleton
+    @Provides
+    fun provideAeadInfoMapper(): Mapper<AeadInfoDto, AeadInfo> = AeadInfoMapper()
+
+}
