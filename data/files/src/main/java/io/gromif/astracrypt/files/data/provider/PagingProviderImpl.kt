a51256aa144fbdff453e95771eb59ba3dfb8ae02
gromif <alex.kobrys@gmail.com>
2025-01-21 18:25:41 +0200

    Implement PagingProvider

---
+++ data/files/src/main/java/io/gromif/astracrypt/files/data/provider/PagingProviderImpl.kt
@@ -0,0 +1,93 @@
+package io.gromif.astracrypt.files.data.provider
+
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import androidx.paging.PagingSource
+import androidx.paging.map
+import io.gromif.astracrypt.files.data.db.FilesDao
+import io.gromif.astracrypt.files.data.db.tuples.PagerTuple
+import io.gromif.astracrypt.files.domain.model.FileItem
+import io.gromif.astracrypt.files.domain.model.FileSource
+import io.gromif.astracrypt.files.domain.model.FileState
+import io.gromif.astracrypt.files.domain.model.FileType
+import io.gromif.astracrypt.files.domain.provider.PagingProvider
+import io.gromif.astracrypt.files.domain.repository.Repository
+import io.gromif.astracrypt.files.domain.repository.SettingsRepository
+import io.gromif.astracrypt.files.domain.util.AeadUtil
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.update
+
+class PagingProviderImpl(
+    private val filesDao: FilesDao,
+    private val pagingConfig: PagingConfig,
+    private val aeadUtil: AeadUtil,
+    private val repository: Repository,
+    private val settingsRepository: SettingsRepository,
+) : PagingProvider<PagingData<FileItem>> {
+    private var pagingSource: PagingSource<Int, PagerTuple>? = null
+    private val searchQueryState = MutableStateFlow<String?>(null)
+    private val searchFolderIdState = MutableStateFlow<List<Long>>(emptyList())
+
+    override fun provide(
+        parentIdState: StateFlow<Long>,
+        isStarredMode: Boolean,
+    ): Flow<PagingData<FileItem>> {
+        return Pager(
+            config = pagingConfig,
+            pagingSourceFactory = {
+                filesDao.listOrderDescAsc(
+                    rootId = parentIdState.value,
+                    isStarredMode = isStarredMode,
+                    query = searchQueryState.value,
+                    rootIdsToSearch = searchFolderIdState.value,
+                    sortingItemType = FileType.Folder.ordinal,
+                    sortingSecondType = 1
+                ).also { pagingSource = it }
+            }
+        ).flow.map { pd ->
+            val aeadInfo = settingsRepository.getAeadInfo()
+            val isEncryptionEnabled = aeadInfo.databaseAeadIndex != -1
+
+            suspend fun decrypt(data: String): String =
+                aeadUtil.decrypt(aeadInfo.databaseAeadIndex, data)
+
+            val pagingData = if (isEncryptionEnabled) pd.map {
+                it.copy(
+                    name = if (aeadInfo.name) decrypt(it.name) else it.name,
+                    preview = it.preview?.let {
+                        if (aeadInfo.preview) decrypt(it) else it
+                    }
+                )
+            } else pd
+            pagingData.map { pagerTuple ->
+                FileItem(
+                    id = pagerTuple.id,
+                    name = pagerTuple.name,
+                    type = FileType.entries[pagerTuple.type],
+                    preview = pagerTuple.preview?.let { 
+                        FileSource(path = it, aeadIndex = pagerTuple.previewAead)
+                    },
+                    state = FileState.entries[pagerTuple.state]
+                )
+            }
+        }
+    }
+
+    override suspend fun setSearchQuery(parentId: Long, query: String?) {
+        val searchQuery = query?.takeIf { it.isNotEmpty() }
+        if (searchQuery == searchQueryState.value) return
+        searchQueryState.update { searchQuery }
+        searchFolderIdState.update {
+            if (searchQuery != null) repository.getFolderIds(parentId) else emptyList()
+        }
+    }
+
+    override fun invalidate() {
+        pagingSource?.invalidate()
+    }
+
+}
---
+++ di/files/src/main/java/io/gromif/astracrypt/files/di/PagingProviderModule.kt
@@ -0,0 +1,40 @@
+package io.gromif.astracrypt.files.di
+
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.scopes.ViewModelScoped
+import io.gromif.astracrypt.files.data.db.FilesDao
+import io.gromif.astracrypt.files.data.provider.PagingProviderImpl
+import io.gromif.astracrypt.files.domain.model.FileItem
+import io.gromif.astracrypt.files.domain.provider.PagingProvider
+import io.gromif.astracrypt.files.domain.repository.Repository
+import io.gromif.astracrypt.files.domain.repository.SettingsRepository
+import io.gromif.astracrypt.files.domain.util.AeadUtil
+
+@Module
+@InstallIn(ViewModelComponent::class)
+internal object PagingProviderModule {
+
+    @ViewModelScoped
+    @Provides
+    fun providePagingProvider(
+        filesDao: FilesDao,
+        aeadUtil: AeadUtil,
+        repository: Repository,
+        settingsRepository: SettingsRepository
+    ): PagingProvider<PagingData<FileItem>> = PagingProviderImpl(
+        filesDao = filesDao,
+        pagingConfig = PagingConfig(
+            pageSize = 10,
+            enablePlaceholders = false
+        ),
+        aeadUtil = aeadUtil,
+        repository = repository,
+        settingsRepository = settingsRepository
+    )
+
+}
