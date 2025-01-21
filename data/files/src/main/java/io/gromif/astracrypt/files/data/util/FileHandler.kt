189e51d95630b9e9443b2d8565302549f3879029
gromif <alex.kobrys@gmail.com>
2025-01-21 18:19:23 +0200

    Add FileHandler

---
+++ data/files/src/main/java/io/gromif/astracrypt/files/data/util/FileHandler.kt
@@ -0,0 +1,96 @@
+package io.gromif.astracrypt.files.data.util
+
+import com.google.crypto.tink.StreamingAead
+import com.nevidimka655.astracrypt.utils.io.Randomizer
+import com.nevidimka655.crypto.tink.data.KeysetManager
+import com.nevidimka655.crypto.tink.domain.KeysetTemplates
+import com.nevidimka655.crypto.tink.extensions.streamingAead
+import io.gromif.astracrypt.files.domain.repository.SettingsRepository
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.isActive
+import java.io.File
+import java.io.InputStream
+import java.io.OutputStream
+
+class FileHandler(
+    private val keysetManager: KeysetManager,
+    private val settingsRepository: SettingsRepository,
+    private val randomizer: Randomizer,
+    filesDir: File
+) {
+    private val dataFolder = "$filesDir/data"
+    private val newRelativePath get() = "${getRandomFolderName()}/${getRandomFileName()}"
+
+    suspend fun writeFile(input: InputStream): String? = coroutineScope {
+        val file = getFilePath(relativePath = newRelativePath)
+        val aead = getFileStreamingAead()
+        val outputStream = getConditionalOutputStream(
+            aead = aead,
+            outputStream = file.outputStream()
+        )
+
+        val buffer = ByteArray(8 * 1024)
+        var loadedSize = input.read(buffer)
+        while (isActive && loadedSize != -1) {
+            outputStream.write(buffer, 0, loadedSize)
+            loadedSize = input.read(buffer)
+        }
+        outputStream.close()
+
+        if (!isActive && file.delete()) null else file.toString()
+    }
+
+    suspend fun writePreview(bytes: ByteArray): String? = coroutineScope {
+        val file = getFilePath(relativePath = newRelativePath)
+        val aead = getPreviewStreamingAead()
+        val outputStream = getConditionalOutputStream(
+            aead = aead,
+            outputStream = file.outputStream()
+        )
+        if (isActive && file.exists()) outputStream.use { it.write(bytes) } else {
+            file.delete()
+            return@coroutineScope null
+        }
+        file.toString()
+    }
+
+    private fun getConditionalOutputStream(
+        aead: StreamingAead?,
+        outputStream: OutputStream
+    ): OutputStream = if (aead != null) {
+        aead.newEncryptingStream(outputStream, keysetManager.associatedData)
+    } else outputStream
+
+    private var cachedFileStreamingAead: StreamingAead? = null
+    suspend fun getFileStreamingAead(): StreamingAead? {
+        return cachedFileStreamingAead ?: run {
+            val aeadIndex = settingsRepository.getAeadInfo().fileAeadIndex
+            KeysetTemplates.Stream.entries.getOrNull(aeadIndex)?.let {
+                keysetManager.getKeyset(tag = "import_file", keyParams = it.params)
+                    .streamingAead()
+            }.also { cachedFileStreamingAead = it }
+        }
+    }
+
+    private var cachedPreviewStreamingAead: StreamingAead? = null
+    suspend fun getPreviewStreamingAead(): StreamingAead? {
+        return cachedPreviewStreamingAead ?: run {
+            val aeadIndex = settingsRepository.getAeadInfo().previewAeadIndex
+            KeysetTemplates.Stream.entries.getOrNull(aeadIndex)?.let {
+                keysetManager.getKeyset(tag = "import_preview", keyParams = it.params)
+                    .streamingAead()
+            }.also { cachedPreviewStreamingAead = it }
+        }
+    }
+
+    private fun getRandomFileName(): String = randomizer.generateUrlSafeString(6)
+    private fun getRandomFolderName(): String = randomizer.generateUrlSafeString(2)
+
+    fun getFilePath(relativePath: String): File {
+        return File("$dataFolder/$relativePath").apply {
+            parentFile!!.mkdirs()
+            createNewFile()
+        }
+    }
+
+}
