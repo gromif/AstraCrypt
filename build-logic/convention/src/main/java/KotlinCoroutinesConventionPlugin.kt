7cbdc1806008330063fee0b0a845a530a72a7050
gromif <alex.kobrys@gmail.com>
2025-01-22 13:00:21 +0200

    Implement `utils:dispatchers` module

--- build-logic/convention/src/main/java/KotlinCoroutinesConventionPlugin.kt
+++ build-logic/convention/src/main/java/KotlinCoroutinesConventionPlugin.kt
@@ -7,6 +7,7 @@ class KotlinCoroutinesConventionPlugin: Plugin<Project> {
     override fun apply(target: Project): Unit = with(target) {
         dependencies {
             "implementation"(libs.findLibrary("kotlin.coroutines").get())
+            "implementation"(project(":core:utils:dispatchers"))
         }
     }
 }
