835e3935a19091fd4d6adb7ae98297cbac768dfd
gromif <alex.kobrys@gmail.com>
2025-01-24 12:28:51 +0200

    Add Dropdown menu support

--- core/navigation/src/main/java/com/nevidimka655/astracrypt/view/navigation/composables/appbar/Toolbar.kt
+++ core/navigation/src/main/java/com/nevidimka655/astracrypt/view/navigation/composables/appbar/Toolbar.kt
@@ -3,7 +3,10 @@ package com.nevidimka655.astracrypt.view.navigation.composables.appbar
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -11,6 +14,10 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import com.nevidimka655.astracrypt.resources.R
@@ -41,12 +48,32 @@ fun ToolbarImpl(
         )
     },
     actions = @Composable {
-        actions?.forEach {
-            IconButton(
-                icon = it.icon,
-                contentDescription = stringResource(id = it.contentDescription),
-                onClick = { onActionPressed(it) }
-            )
+        if (actions != null) {
+            val basicActions = remember { actions.take(2) }
+            basicActions.forEach {
+                IconButton(
+                    icon = it.icon,
+                    contentDescription = stringResource(id = it.contentDescription),
+                ) { onActionPressed(it) }
+            }
+            if (actions.size > 2) {
+                val actionsInDropdownMenu = remember { actions.drop(2) }
+                var expanded by remember { mutableStateOf(false) }
+                DropdownMenu(expanded, onDismissRequest = { expanded = expanded.not() }) {
+                    actionsInDropdownMenu.forEach {
+                        DropdownMenuItem(
+                            text = { Text(stringResource(it.contentDescription)) },
+                            onClick = {
+                                onActionPressed(it)
+                                expanded = expanded.not()
+                            }
+                        )
+                    }
+                }
+                IconButton(icon = Icons.Default.MoreVert, contentDescription = null) {
+                    expanded = expanded.not()
+                }
+            }
         }
     },
     isContextual = isContextual,
