2b470d6e4cc820c129f4bbe016c550bab88eca11
gromif <alex.kobrys@gmail.com>
2025-01-28 12:02:59 +0200

    Decouple fileItems generation

--- features/files/presentation/src/main/java/io/gromif/astracrypt/files/shared/FakeData.kt
+++ features/files/presentation/src/main/java/io/gromif/astracrypt/files/shared/FakeData.kt
@@ -6,24 +6,25 @@ import io.gromif.astracrypt.files.domain.model.FileType
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 
+// fake data for preview
 internal object FakeData {
 
     fun paging(): Flow<PagingData<FileItem>> {
-        // create list of fake data for preview
-        val fakeData = List(10) {
-            var isFolder = it <= 4
-            FileItem(
-                id = it.toLong(),
-                name = "Item $it",
-                type = if (isFolder) FileType.Folder else FileType.entries.random(),
-                isFolder = isFolder,
-                isFile = isFolder.not()
-            )
-        }
         // create pagingData from a list of fake data
-        val pagingData = PagingData.from(fakeData)
+        val pagingData = PagingData.from(fileItems())
         // pass pagingData containing fake data to a MutableStateFlow
         return MutableStateFlow(pagingData)
+    }
+
+    fun fileItems(): List<FileItem> = List(10) {
+        var isFolder = it <= 4
+        FileItem(
+            id = it.toLong(),
+            name = "Item $it",
+            type = if (isFolder) FileType.Folder else FileType.entries.random(),
+            isFolder = isFolder,
+            isFile = isFolder.not()
+        )
     }
 
 }
