8aa898509cba61abe20fb68f4f5ffbc40aff2441
gromif <alex.kobrys@gmail.com>
2025-01-29 17:22:51 +0200

    Add the `ExternalIconUtil` class to group preview-loading functionality

---
+++ features/profile/data/src/main/java/io/gromif/astracrypt/profile/data/util/ExternalIconUtil.kt
@@ -0,0 +1,32 @@
+package io.gromif.astracrypt.profile.data.util
+
+import android.net.Uri
+import com.nevidimka655.astracrypt.utils.io.BitmapCompressor
+import com.nevidimka655.crypto.tink.domain.KeysetTemplates
+
+class ExternalIconUtil(
+    private val previewUtil: PreviewUtil,
+    private val bitmapCompressor: BitmapCompressor,
+    private val fileUtil: FileUtil,
+) {
+
+    suspend fun saveFromUri(
+        uri: Uri,
+        quality: Int,
+        size: Int,
+        aead: Int
+    ) {
+        val bitmap = previewUtil(uri, size)
+        val compressedPreview = bitmapCompressor(bitmap, quality)
+        fileUtil.recreate()
+        fileUtil.write(
+            bytes = compressedPreview,
+            aeadTemplate = KeysetTemplates.Stream.entries.getOrNull(aead)
+        )
+    }
+
+    suspend fun changeAead(oldAead: KeysetTemplates.Stream?, newAead: KeysetTemplates.Stream?) {
+        fileUtil.changeAead(oldAead, newAead)
+    }
+
+}
