cce0679254052606e00314d314cbe062a62bc077
gromif <alex.kobrys@gmail.com>
2025-01-29 17:23:10 +0200

    Implement `Repository` interface

---
+++ features/profile/data/src/main/java/io/gromif/astracrypt/profile/data/repository/RepositoryImpl.kt
@@ -0,0 +1,36 @@
+package io.gromif.astracrypt.profile.data.repository
+
+import android.net.Uri
+import com.nevidimka655.astracrypt.utils.Mapper
+import com.nevidimka655.crypto.tink.domain.KeysetTemplates
+import io.gromif.astracrypt.profile.data.util.ExternalIconUtil
+import io.gromif.astracrypt.profile.domain.repository.Repository
+
+class RepositoryImpl(
+    private val externalIconUtil: ExternalIconUtil,
+    private val uriMapper: Mapper<String, Uri>,
+) : Repository {
+
+    override suspend fun importAvatar(
+        aead: Int,
+        path: String,
+        quality: Int,
+        size: Int,
+    ) {
+        val uri = uriMapper(path)
+        externalIconUtil.saveFromUri(
+            uri = uri,
+            quality = quality,
+            size = size,
+            aead = aead
+        )
+    }
+
+    override suspend fun changeAvatarAead(oldAead: Int, newAead: Int) {
+        externalIconUtil.changeAead(
+            oldAead = KeysetTemplates.Stream.entries.getOrNull(oldAead),
+            newAead = KeysetTemplates.Stream.entries.getOrNull(newAead)
+        )
+    }
+
+}
