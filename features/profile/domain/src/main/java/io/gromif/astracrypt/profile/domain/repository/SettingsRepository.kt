6b0b379c4294926cb2f7913a3c2e8b9612929582
gromif <alex.kobrys@gmail.com>
2025-01-29 17:05:03 +0200

    Add `SettingsRepository` model

---
+++ features/profile/domain/src/main/java/io/gromif/astracrypt/profile/domain/repository/SettingsRepository.kt
@@ -0,0 +1,18 @@
+package io.gromif.astracrypt.profile.domain.repository
+
+import io.gromif.astracrypt.profile.domain.model.Profile
+import kotlinx.coroutines.flow.Flow
+
+interface SettingsRepository {
+
+    val profileFlow: Flow<Profile>
+
+    suspend fun getProfile(): Profile
+    suspend fun setProfile(profile: Profile)
+
+    suspend fun getAvatarAead(): Int
+    suspend fun setAvatarAead(aead: Int)
+
+    suspend fun setAead(aead: Int)
+
+}
