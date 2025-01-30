d05cd30bc372412a9752da88c842034ca05f6d04
gromif <alex.kobrys@gmail.com>
2025-01-31 00:31:06 +0200

    refactor(auth): bind mappers to the `ViewModelScope`

--- features/auth/di/src/main/java/com/nevidimka655/astracrypt/auth/di/MapperModule.kt
+++ features/auth/di/src/main/java/com/nevidimka655/astracrypt/auth/di/MapperModule.kt
@@ -9,14 +9,17 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.scopes.ViewModelScoped
 
 @Module
 @InstallIn(ViewModelComponent::class)
 internal object MapperModule {
 
+    @ViewModelScoped
     @Provides
     fun provideAuthDtoMapper(): Mapper<Auth, AuthDto> = AuthDtoMapper()
 
+    @ViewModelScoped
     @Provides
     fun provideAuthMapper(): Mapper<AuthDto, Auth> = AuthMapper()
 
