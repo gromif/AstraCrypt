ee192ef13a7f75e2cf90f3b54559f2533424ad86
gromif <alex.kobrys@gmail.com>
2025-02-02 11:46:10 +0200

    refactor(files): remove unused DAO-related DTOs

--- features/files/data/src/main/java/io/gromif/astracrypt/files/data/db/tuples/OpenTuple.kt
+++
@@ -1,8 +0,0 @@
-package io.gromif.astracrypt.files.data.db.tuples
-
-import androidx.room.ColumnInfo
-
-data class OpenTuple(
-    @ColumnInfo(name = "name") val name: String,
-    @ColumnInfo(name = "file") val path: String
-)
