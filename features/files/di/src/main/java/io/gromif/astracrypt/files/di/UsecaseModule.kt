0f536c8861f24b0171c1037d18ca9f3bbe0edace
gromif <alex.kobrys@gmail.com>
2025-02-02 12:49:07 +0200

    refactor(files): remove use cases' VM scoping

--- features/files/di/src/main/java/io/gromif/astracrypt/files/di/UsecaseModule.kt
+++ features/files/di/src/main/java/io/gromif/astracrypt/files/di/UsecaseModule.kt
@@ -4,7 +4,6 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
-import dagger.hilt.android.scopes.ViewModelScoped
 import io.gromif.astracrypt.files.domain.repository.Repository
 import io.gromif.astracrypt.files.domain.repository.SettingsRepository
 import io.gromif.astracrypt.files.domain.usecase.CreateFolderUseCase
@@ -20,42 +19,34 @@ import io.gromif.astracrypt.files.domain.usecase.SetStarredUseCase
 @InstallIn(ViewModelComponent::class)
 internal object UsecaseModule {
 
-    @ViewModelScoped
     @Provides
     fun provideCreateFolderUseCase(repository: Repository): CreateFolderUseCase =
         CreateFolderUseCase(repository = repository)
 
-    @ViewModelScoped
     @Provides
     fun provideDeleteUseCase(repository: Repository): DeleteUseCase =
         DeleteUseCase(repository = repository)
 
-    @ViewModelScoped
     @Provides
     fun provideGetListViewModeUseCase(settingsRepository: SettingsRepository): GetListViewModeUseCase =
         GetListViewModeUseCase(settingsRepository = settingsRepository)
 
-    @ViewModelScoped
     @Provides
     fun provideMoveUseCase(repository: Repository): MoveUseCase =
         MoveUseCase(repository = repository)
 
-    @ViewModelScoped
     @Provides
     fun provideRenameUseCase(repository: Repository): RenameUseCase =
         RenameUseCase(repository = repository)
 
-    @ViewModelScoped
     @Provides
     fun provideSetStarredUseCase(repository: Repository): SetStarredUseCase =
         SetStarredUseCase(repository = repository)
 
-    @ViewModelScoped
     @Provides
     fun provideGetRecentItemsUseCase(repository: Repository): GetRecentItemsUseCase =
         GetRecentItemsUseCase(repository = repository)
 
-    @ViewModelScoped
     @Provides
     fun provideGetItemDetailsUseCase(repository: Repository): GetItemDetailsUseCase =
         GetItemDetailsUseCase(repository = repository)
