ae02d2f3dfaac36d0d1331911471c41709a96aa0
gromif <alex.kobrys@gmail.com>
2025-02-07 22:07:14 +0200

    feat(utils): add the `openExternally` extension

---
+++ core/utils/src/main/java/com/nevidimka655/astracrypt/utils/app/ext/Context.kt
@@ -0,0 +1,7 @@
+package com.nevidimka655.astracrypt.utils.app.ext
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+
+fun Context.openExternally(uri: Uri) = startActivity(Intent(Intent.ACTION_VIEW, uri))
