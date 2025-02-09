d717bc36a5b64572d05e90eaad95bd6195648891
gromif <alex.kobrys@gmail.com>
2025-02-09 12:19:39 +0200

    feat(about): add the `fdroid` variant-module

---
+++ features/settings/about/fdroid/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    alias(libs.plugins.astracrypt.android.library)
+    alias(libs.plugins.astracrypt.android.library.compose)
+}
+
+android {
+    namespace = "io.gromif.astracrypt.settings.about"
+}
+
+dependencies {
+    implementation(projects.features.settings.about)
+    implementation(projects.core.resources)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
