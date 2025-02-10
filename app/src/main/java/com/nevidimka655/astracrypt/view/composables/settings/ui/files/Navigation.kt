e67a0a23d9342f186315d12fc8d08eccfe8d36c9
gromif <alex.kobrys@gmail.com>
2025-02-10 18:32:06 +0200

    fix(lab): update UiState in `UiStateHandler`

--- app/src/main/java/com/nevidimka655/astracrypt/view/composables/settings/ui/files/Navigation.kt
+++ app/src/main/java/com/nevidimka655/astracrypt/view/composables/settings/ui/files/Navigation.kt
@@ -6,6 +6,7 @@ import com.nevidimka655.astracrypt.resources.R
 import com.nevidimka655.ui.compose_core.wrappers.TextWrap
 import io.gromif.astracrypt.presentation.navigation.Route
 import io.gromif.astracrypt.presentation.navigation.models.UiState
+import io.gromif.astracrypt.presentation.navigation.shared.UiStateHandler
 
 private val SettingsUiFilesUiState = UiState(
     toolbar = UiState.Toolbar(
@@ -16,7 +17,7 @@ private val SettingsUiFilesUiState = UiState(
 fun NavGraphBuilder.settingsUiFiles(
     onUiStateChange: (UiState) -> Unit
 ) = composable<Route.SettingsUiFiles> {
-    onUiStateChange(SettingsUiFilesUiState)
+    UiStateHandler { onUiStateChange(SettingsUiFilesUiState) }
     //val vm: SettingsUiFilesViewModel = hiltViewModel()
 
     /*SettingUiFilesScreen(
