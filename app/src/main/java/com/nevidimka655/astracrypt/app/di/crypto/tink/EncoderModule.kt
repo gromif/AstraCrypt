6873de3a7e8f77551908fe2c6e631b11aaac15b3
gromif <alex.kobrys@gmail.com>
2025-02-14 20:35:02 +0200

    chore(tink): annotate encoders with `@Singleton`

--- app/src/main/java/com/nevidimka655/astracrypt/app/di/crypto/tink/EncoderModule.kt
+++ app/src/main/java/com/nevidimka655/astracrypt/app/di/crypto/tink/EncoderModule.kt
@@ -6,14 +6,17 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import io.gromif.crypto.tink.core.encoders.Base64Encoder
 import io.gromif.crypto.tink.core.encoders.HexEncoder
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object EncoderModule {
 
+    @Singleton
     @Provides
     fun provideHexService() = HexEncoder()
 
+    @Singleton
     @Provides
     fun provideBase64Service() = Base64Encoder()
 }
