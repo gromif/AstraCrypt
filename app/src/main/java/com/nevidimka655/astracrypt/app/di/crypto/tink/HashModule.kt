2df178eb93800096c76246fbc60cd9bfb778604a
gromif <alex.kobrys@gmail.com>
2025-02-14 20:37:29 +0200

    chore(tink): scope hash-utilities with `@Singleton`

--- app/src/main/java/com/nevidimka655/astracrypt/app/di/crypto/tink/HashModule.kt
+++ app/src/main/java/com/nevidimka655/astracrypt/app/di/crypto/tink/HashModule.kt
@@ -6,13 +6,17 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import io.gromif.crypto.tink.core.hash.Sha256Util
 import io.gromif.crypto.tink.core.hash.Sha384Util
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object HashModule {
+
+    @Singleton
     @Provides
     fun provideSha256Service() = Sha256Util()
 
+    @Singleton
     @Provides
     fun provideSha384Service() = Sha384Util()
 }
