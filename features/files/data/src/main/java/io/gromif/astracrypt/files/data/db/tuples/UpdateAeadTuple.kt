535783b77f2368d3896c9d3c080f13f4437802cf
gromif <alex.kobrys@gmail.com>
2025-02-18 17:30:21 +0200

    refactor(files): match database fields nullability

--- features/files/data/src/main/java/io/gromif/astracrypt/files/data/db/tuples/UpdateAeadTuple.kt
+++ features/files/data/src/main/java/io/gromif/astracrypt/files/data/db/tuples/UpdateAeadTuple.kt
@@ -3,7 +3,7 @@ package io.gromif.astracrypt.files.data.db.tuples
 data class UpdateAeadTuple(
     val id: Long,
     val name: String,
-    val preview: String,
-    val file: String,
-    val flags: String
+    val preview: String?,
+    val file: String?,
+    val flags: String?
 )
