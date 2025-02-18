a5909b73137429fb5071616333a038de9f3e8ae2
gromif <alex.kobrys@gmail.com>
2025-02-18 12:22:04 +0200

    refactor(files): remove unnecessary entity annotations

--- features/files/data/src/main/java/io/gromif/astracrypt/files/data/db/FilesEntity.kt
+++ features/files/data/src/main/java/io/gromif/astracrypt/files/data/db/FilesEntity.kt
@@ -1,6 +1,5 @@
 package io.gromif.astracrypt.files.data.db
 
-import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.Index
 import androidx.room.PrimaryKey
@@ -17,34 +16,15 @@ import io.gromif.astracrypt.files.domain.model.ItemType
 data class FilesEntity(
     @PrimaryKey(autoGenerate = true)
     val id: Long = 0,
-    @ColumnInfo(name = "parent")
     val parent: Long = 0,
-
-    @ColumnInfo(name = "name")
     val name: String = "",
-
-    @ColumnInfo(name = "state")
     val state: ItemState = ItemState.Default,
-
-    @ColumnInfo(name = "type")
     val type: ItemType = ItemType.Folder,
-
-    @ColumnInfo(name = "file")
     val file: String? = null,
-    @ColumnInfo(name = "fileAead")
     val fileAead: Int = -1,
-
-    @ColumnInfo(name = "preview")
     val preview: String? = null,
-    @ColumnInfo(name = "previewAead")
     val previewAead: Int = -1,
-
-    @ColumnInfo(name = "flags")
     val flags: String? = null,
-
-    @ColumnInfo(name = "time")
     val time: Long = 0,
-
-    @ColumnInfo(name = "size")
     val size: Long = 0,
 )
