baf8c8397398e01fe92cd9ef567821872b17ef4e
gromif <alex.kobrys@gmail.com>
2025-03-09 12:15:27 +0200

    fix(about-gp): update developer name

--- features/about/google-play/src/main/java/io/gromif/astracrypt/settings/about/Config.kt
+++ features/about/google-play/src/main/java/io/gromif/astracrypt/settings/about/Config.kt
@@ -7,7 +7,7 @@ internal object Config {
         "https://play.google.com/store/apps/details?id=com.nevidimka655.astracrypt".toUri()
 
     val MORE_APPS_PAGE =
-        "https://play.google.com/store/apps/developer?id=Nevidimka655".toUri()
+        "https://play.google.com/store/apps/developer?id=gromif".toUri()
 
     const val EMAIL = "alex.kobrys@gmail.com"
 }
