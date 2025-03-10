c2e2e192d350a788af2660a12d5821ffa5ee4574
gromif <alex.kobrys@gmail.com>
2025-03-10 16:48:05 +0200

    perf(files): annotate `AeadSettingsScreenParams` with `Stable`

--- features/files/presentation/src/main/java/io/gromif/astracrypt/files/settings/aead/AeadSettingsScreenParams.kt
+++ features/files/presentation/src/main/java/io/gromif/astracrypt/files/settings/aead/AeadSettingsScreenParams.kt
@@ -1,7 +1,9 @@
 package io.gromif.astracrypt.files.settings.aead
 
+import androidx.compose.runtime.Stable
 import io.gromif.astracrypt.files.domain.model.AeadInfo
 
+@Stable
 internal data class AeadSettingsScreenParams(
     val aeadInfo: AeadInfo = AeadInfo(),
 
