27ad00b6bbefd5aee6b08497d8580c6316f2078f
gromif <alex.kobrys@gmail.com>
2025-03-10 16:47:16 +0200

    perf(navigation): annotate `HostStateHolder` with `Stable`

--- ui/navigation/src/main/java/io/gromif/astracrypt/presentation/navigation/models/HostStateHolder.kt
+++ ui/navigation/src/main/java/io/gromif/astracrypt/presentation/navigation/models/HostStateHolder.kt
@@ -1,8 +1,10 @@
 package io.gromif.astracrypt.presentation.navigation.models
 
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.Stable
 import kotlinx.coroutines.flow.StateFlow
 
+@Stable
 data class HostStateHolder(
     val dynamicThemeState: Boolean,
     val snackbarHostState: SnackbarHostState,
