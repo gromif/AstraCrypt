33705ccc5fc515c8f3f4b7b9b5881b1f61b65a4d
gromif <alex.kobrys@gmail.com>
2025-03-10 17:31:52 +0200

    feat(files): enhance default security settings

--- features/files/domain/src/main/java/io/gromif/astracrypt/files/domain/model/AeadInfo.kt
+++ features/files/domain/src/main/java/io/gromif/astracrypt/files/domain/model/AeadInfo.kt
@@ -1,11 +1,11 @@
 package io.gromif.astracrypt.files.domain.model
 
 data class AeadInfo(
-    val fileMode: AeadMode = AeadMode.None,
-    val previewMode: AeadMode = AeadMode.None,
-    val databaseMode: AeadMode = AeadMode.None,
+    val fileMode: AeadMode = AeadMode.Template(id = 6, name = ""),
+    val previewMode: AeadMode = AeadMode.Template(id = 7, name = ""),
+    val databaseMode: AeadMode = AeadMode.Template(id = 10, name = ""),
     val name: Boolean = false,
     val preview: Boolean = false,
     val file: Boolean = false,
-    val flag: Boolean = false
+    val flag: Boolean = true
 )
