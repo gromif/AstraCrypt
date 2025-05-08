2a52e3faa22cbc7283b2cd2e9ba4b5d44eef76fc
gromif <alexander.kobrys@proton.me>
2025-05-08 13:45:21 +0300

    feat(auth): add shared callbacks

---
+++ features/auth/presentation/src/main/java/io/gromif/astracrypt/auth/presentation/shared/onAuthType.kt
@@ -0,0 +1,12 @@
+@file:Suppress("ClassName")
+
+package io.gromif.astracrypt.auth.presentation.shared
+
+import androidx.compose.runtime.Immutable
+
+@Immutable
+interface onAuthType {
+
+    fun onPassword()
+
+}
