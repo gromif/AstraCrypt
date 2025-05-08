e183ad9af0aabf71ffb61382be8ae08e710da1ef
gromif <alexander.kobrys@proton.me>
2025-05-08 11:35:42 +0300

    feat(auth): define `Repository` abstraction

---
+++ features/auth/domain/src/main/java/io/gromif/astracrypt/auth/domain/repository/Repository.kt
@@ -0,0 +1,22 @@
+package io.gromif.astracrypt.auth.domain.repository
+
+import io.gromif.astracrypt.auth.domain.model.Timeout
+import kotlinx.coroutines.flow.Flow
+
+interface Repository {
+
+    fun getAuthStateFlow(): Flow<Boolean>
+
+    fun getSkinStateFlow(): Flow<Boolean>
+
+    fun verifyTimeout(
+        currentTime: Long,
+        lastActiveTime: Long,
+        timeout: Timeout
+    )
+
+    suspend fun verifyAuth(savedHash: ByteArray, secret: String): Boolean
+
+    suspend fun verifySkin(savedHash: ByteArray, secret: String): Boolean
+
+}
