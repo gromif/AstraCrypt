a1aa1110b8fdd83fb433180717a05a51eb0b8d17
gromif <alexander.kobrys@proton.me>
2025-05-17 13:30:14 +0300

    refactor(data): implement `StorageNavigator` abstraction

---
+++ features/files/data/src/test/kotlin/io/gromif/astracrypt/files/data/repository/DefaultStorageNavigatorTest.kt
@@ -0,0 +1,88 @@
+package io.gromif.astracrypt.files.data.repository
+
+import io.gromif.astracrypt.files.domain.repository.StorageNavigator
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+
+private typealias Folder = StorageNavigator.Folder
+
+class DefaultStorageNavigatorTest {
+    private lateinit var storageNavigator: StorageNavigator
+    private val predefinedBackStack = (1..5).map {
+        Folder(it.toLong(), "Folder$it")
+    }
+
+    @Before
+    fun setUp() {
+        storageNavigator = DefaultStorageNavigator()
+    }
+
+    @Test
+    fun `getCurrentFolder should return the last folder when backStack is not empty`() {
+        val targetFolder = Folder(id = 24, name = "Test")
+
+        storageNavigator.push(targetFolder)
+        val result = storageNavigator.getCurrentFolder()
+
+        Assert.assertSame(targetFolder, result)
+    }
+
+    @Test
+    fun `getCurrentFolder should return the rootFolder when backStack is empty`() {
+        val targetRootFolder = Folder(id = 0, name = "")
+        val result = storageNavigator.getCurrentFolder()
+        Assert.assertEquals(targetRootFolder, result)
+    }
+
+    @Test
+    fun `swapBackStackWith should swap the backStack with targetBackStack`() = runTest {
+        storageNavigator.swapBackStackWith(predefinedBackStack)
+        val result = storageNavigator.getBackStackFlow().first()
+
+        Assert.assertSame(predefinedBackStack, result)
+    }
+
+    @Test
+    fun `push should add the entry to the backStack when it's not in the backStack`() = runTest {
+        val targetFolder = Folder(id = 24, name = "Test")
+
+        storageNavigator.push(targetFolder)
+        val result = storageNavigator.getBackStackFlow().first()
+
+        Assert.assertSame(targetFolder, result.first())
+    }
+
+    @Test
+    fun `push should truncate the backStack when the record is in the backStack`() = runTest {
+        val targetFolder = predefinedBackStack[2]
+        val targetBackStack = predefinedBackStack.toMutableList().apply {
+            val entriesToRemove = subList(fromIndex = 3, toIndex = size)
+            removeAll(entriesToRemove)
+        }.toTypedArray()
+
+        storageNavigator.swapBackStackWith(predefinedBackStack)
+        storageNavigator.push(targetFolder)
+        val result = storageNavigator.getBackStackFlow().first().toTypedArray()
+
+        val contentMatchesCondition = result.contentEquals(targetBackStack)
+        Assert.assertTrue(contentMatchesCondition)
+    }
+
+    @Test
+    fun `pop should remove the last folder from the backStack`() = runTest {
+        val targetBackStack = predefinedBackStack.toMutableList().apply {
+            removeAt(lastIndex)
+        }.toTypedArray()
+
+        storageNavigator.swapBackStackWith(predefinedBackStack)
+        storageNavigator.pop()
+        val result = storageNavigator.getBackStackFlow().first().toTypedArray()
+
+        val contentMatchesCondition = result.contentEquals(targetBackStack)
+        Assert.assertTrue(contentMatchesCondition)
+    }
+
+}
