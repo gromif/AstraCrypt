89086de04ccdeead1e10c3d951040dea842df231
gromif <alexander.kobrys@proton.me>
2025-05-17 12:35:45 +0300

    refactor(domain): introduce `StorageNavigator` abstraction

---
+++ features/files/domain/src/main/java/io/gromif/astracrypt/files/domain/repository/StorageNavigator.kt
@@ -0,0 +1,21 @@
+package io.gromif.astracrypt.files.domain.repository
+
+import kotlinx.coroutines.flow.Flow
+
+interface StorageNavigator {
+
+    fun getBackStackFlow(): Flow<List<Folder>>
+
+    fun getCurrentFolder(): Folder
+
+    fun push(folder: Folder)
+
+    fun pop()
+
+    fun swapBackStackWith(targetBackStack: List<Folder>)
+
+    data class Folder(
+        val id: Long,
+        val name: String
+    )
+}
