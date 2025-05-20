8f0e51a00bc2cb111a0b5570e5cc572a8917c966
gromif <alexander.kobrys@proton.me>
2025-05-20 18:09:36 +0300

    refactor(domain): remove fully-qualified package references

--- features/files/domain/src/main/java/io/gromif/astracrypt/files/domain/usecase/GetDataFlowUseCase.kt
+++ features/files/domain/src/main/java/io/gromif/astracrypt/files/domain/usecase/GetDataFlowUseCase.kt
@@ -1,23 +1,30 @@
 package io.gromif.astracrypt.files.domain.usecase
 
+import io.gromif.astracrypt.files.domain.repository.DataSource
+import io.gromif.astracrypt.files.domain.repository.StorageNavigator
+import io.gromif.astracrypt.files.domain.usecase.navigator.GetCurrentNavFolderFlowUseCase
+import io.gromif.astracrypt.files.domain.usecase.search.GetSearchRequestFlow
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flatMapLatest
 
-@OptIn(kotlinx.coroutines.ExperimentalCoroutinesApi::class)
+@OptIn(ExperimentalCoroutinesApi::class)
 class GetDataFlowUseCase<T>(
-    private val getCurrentNavFolderFlowUseCase: io.gromif.astracrypt.files.domain.usecase.navigator.GetCurrentNavFolderFlowUseCase,
-    private val getSearchRequestFlow: io.gromif.astracrypt.files.domain.usecase.search.GetSearchRequestFlow,
-    private val dataSource: io.gromif.astracrypt.files.domain.repository.DataSource<T>
+    private val getCurrentNavFolderFlowUseCase: GetCurrentNavFolderFlowUseCase,
+    private val getSearchRequestFlow: GetSearchRequestFlow,
+    private val dataSource: DataSource<T>
 ) {
 
-    operator fun invoke(): kotlinx.coroutines.flow.Flow<T> {
+    operator fun invoke(): Flow<T> {
         val currentNavFolderFlow = getCurrentNavFolderFlowUseCase()
         val searchRequestFlow = getSearchRequestFlow()
 
-        return kotlinx.coroutines.flow.combine(
+        return combine(
             currentNavFolderFlow,
             searchRequestFlow
         ) { it }.flatMapLatest { resultArray ->
-            val currentNavFolder = resultArray[0] as io.gromif.astracrypt.files.domain.repository.StorageNavigator.Folder
+            val currentNavFolder = resultArray[0] as StorageNavigator.Folder
             val searchRequest = resultArray[1] as String?
 
             dataSource.getDataFlow(
