428e1cedf1e2bfcffb7d3fda88249b4de513655f
gromif <alexander.kobrys@proton.me>
2025-05-20 19:23:35 +0300

    refactor(data): unify the process of `Pager` creation

--- features/files/data/src/main/java/io/gromif/astracrypt/files/data/repository/dataSource/DefaultDataSource.kt
+++ features/files/data/src/main/java/io/gromif/astracrypt/files/data/repository/dataSource/DefaultDataSource.kt
@@ -1,21 +1,15 @@
 package io.gromif.astracrypt.files.data.repository.dataSource
 
-import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
-import androidx.paging.map
 import io.gromif.astracrypt.files.data.db.FilesDao
 import io.gromif.astracrypt.files.data.util.AeadHandler
 import io.gromif.astracrypt.files.domain.model.AeadInfo
-import io.gromif.astracrypt.files.domain.model.AeadMode
-import io.gromif.astracrypt.files.domain.model.FileSource
 import io.gromif.astracrypt.files.domain.model.Item
-import io.gromif.astracrypt.files.domain.model.ItemState
 import io.gromif.astracrypt.files.domain.model.ItemType
 import io.gromif.astracrypt.files.domain.repository.DataSource
 import io.gromif.astracrypt.files.domain.repository.search.SearchStrategy
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
 
 class DefaultDataSource(
     private val defaultSearchStrategy: SearchStrategy<Long, List<Long>>,
@@ -36,33 +30,18 @@ class DefaultDataSource(
             emptyList()
         }
         val sortSecondType = if (aeadInfo.name) ItemType.Folder.ordinal else ItemType.Other.ordinal
-        return Pager(
-            config = pagingConfig,
-            pagingSourceFactory = {
-                filesDao.listDefault(
-                    rootId = folderId,
-                    query = searchQuery,
-                    rootIdsToSearch = rootIdsToSearch,
-                    sortingItemType = ItemType.Folder.ordinal,
-                    sortingSecondType = sortSecondType
-                )
-            }
-        ).flow.map { pd ->
-            pd.map { pagerTuple ->
-                val databaseMode = aeadInfo.databaseMode
-                val data = if (databaseMode is AeadMode.Template) {
-                    aeadHandler.decryptPagerTuple(aeadInfo, databaseMode, pagerTuple)
-                } else pagerTuple
-                Item(
-                    id = data.id,
-                    name = data.name,
-                    type = data.type,
-                    preview = data.preview?.let {
-                        FileSource(path = it, aeadIndex = data.previewAead)
-                    },
-                    state = ItemState.entries[data.state]
-                )
-            }
+        return DefaultPagerFactory(
+            pagingConfig = pagingConfig,
+            aeadHandler = aeadHandler,
+            aeadInfo = aeadInfo
+        ) {
+            filesDao.listDefault(
+                rootId = folderId,
+                query = searchQuery,
+                rootIdsToSearch = rootIdsToSearch,
+                sortingItemType = ItemType.Folder.ordinal,
+                sortingSecondType = sortSecondType
+            )
         }
     }
 
--- features/files/data/src/main/java/io/gromif/astracrypt/files/data/repository/dataSource/StarredDataSource.kt
+++ features/files/data/src/main/java/io/gromif/astracrypt/files/data/repository/dataSource/StarredDataSource.kt
@@ -1,20 +1,14 @@
 package io.gromif.astracrypt.files.data.repository.dataSource
 
-import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
-import androidx.paging.map
 import io.gromif.astracrypt.files.data.db.FilesDao
 import io.gromif.astracrypt.files.data.util.AeadHandler
 import io.gromif.astracrypt.files.domain.model.AeadInfo
-import io.gromif.astracrypt.files.domain.model.AeadMode
-import io.gromif.astracrypt.files.domain.model.FileSource
 import io.gromif.astracrypt.files.domain.model.Item
-import io.gromif.astracrypt.files.domain.model.ItemState
 import io.gromif.astracrypt.files.domain.model.ItemType
 import io.gromif.astracrypt.files.domain.repository.DataSource
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
 
 class StarredDataSource(
     private val filesDao: FilesDao,
@@ -29,31 +23,16 @@ class StarredDataSource(
     ): Flow<PagingData<Item>> {
         val searchQuery = searchRequest?.takeIf { it.isNotEmpty() }
         val sortSecondType = if (aeadInfo.name) ItemType.Folder.ordinal else ItemType.Other.ordinal
-        return Pager(
-            config = pagingConfig,
-            pagingSourceFactory = {
-                filesDao.listStarred(
-                    query = searchQuery,
-                    sortingItemType = ItemType.Folder.ordinal,
-                    sortingSecondType = sortSecondType
-                )
-            }
-        ).flow.map { pd ->
-            pd.map { pagerTuple ->
-                val databaseMode = aeadInfo.databaseMode
-                val data = if (databaseMode is AeadMode.Template) {
-                    aeadHandler.decryptPagerTuple(aeadInfo, databaseMode, pagerTuple)
-                } else pagerTuple
-                Item(
-                    id = data.id,
-                    name = data.name,
-                    type = data.type,
-                    preview = data.preview?.let {
-                        FileSource(path = it, aeadIndex = data.previewAead)
-                    },
-                    state = ItemState.entries[data.state]
-                )
-            }
+        return DefaultPagerFactory(
+            pagingConfig = pagingConfig,
+            aeadHandler = aeadHandler,
+            aeadInfo = aeadInfo
+        ) {
+            filesDao.listStarred(
+                query = searchQuery,
+                sortingItemType = ItemType.Folder.ordinal,
+                sortingSecondType = sortSecondType
+            )
         }
     }
 
