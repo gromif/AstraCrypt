af876f35e81a4a2014e7e79d0e49e33e646fd9f0
gromif <alexander.kobrys@proton.me>
2025-05-20 14:23:33 +0300

    refactor(data): introduce `StarredDataSource`

---
+++ features/files/data/src/main/java/io/gromif/astracrypt/files/data/repository/dataSource/StarredDataSource.kt
@@ -0,0 +1,87 @@
+package io.gromif.astracrypt.files.data.repository.dataSource
+
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import androidx.paging.PagingSource
+import androidx.paging.map
+import io.gromif.astracrypt.files.data.db.FilesDao
+import io.gromif.astracrypt.files.data.db.tuples.PagerTuple
+import io.gromif.astracrypt.files.data.util.AeadHandler
+import io.gromif.astracrypt.files.domain.model.AeadMode
+import io.gromif.astracrypt.files.domain.model.FileSource
+import io.gromif.astracrypt.files.domain.model.Item
+import io.gromif.astracrypt.files.domain.model.ItemState
+import io.gromif.astracrypt.files.domain.model.ItemType
+import io.gromif.astracrypt.files.domain.repository.AeadSettingsRepository
+import io.gromif.astracrypt.files.domain.repository.DataSource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.emptyFlow
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.update
+
+class StarredDataSource(
+    private val filesDao: FilesDao,
+    private val pagingConfig: PagingConfig,
+    private val aeadHandler: AeadHandler,
+    private val aeadSettingsRepository: AeadSettingsRepository,
+) : DataSource<PagingData<Item>> {
+    private var pagingSource: PagingSource<Int, PagerTuple>? = null
+    private val folderIdState = MutableStateFlow<Long>(0)
+    private var sortingSecondType = MutableStateFlow(1)
+
+    private fun createPagerFlow(
+        pagingSource: () -> PagingSource<Int, PagerTuple>
+    ): Flow<PagingData<Item>> {
+        val aeadInfoFlow = aeadSettingsRepository.getAeadInfoFlow().onEach {
+            sortingSecondType.emit(if (it.name) 6 else 1)
+            invalidate()
+        }
+        return Pager(
+            config = pagingConfig,
+            pagingSourceFactory = pagingSource
+        ).flow.combine(aeadInfoFlow) { pd, aeadInfo ->
+            pd.map { pagerTuple ->
+                val databaseMode = aeadInfo.databaseMode
+                val data = if (databaseMode is AeadMode.Template) {
+                    aeadHandler.decryptPagerTuple(aeadInfo, databaseMode, pagerTuple)
+                } else pagerTuple
+                Item(
+                    id = data.id,
+                    name = data.name,
+                    type = data.type,
+                    preview = data.preview?.let {
+                        FileSource(path = it, aeadIndex = data.previewAead)
+                    },
+                    state = ItemState.entries[data.state]
+                )
+            }
+        }
+    }
+
+    override suspend fun getDataFlow(searchRequest: String?): Flow<PagingData<Item>> {
+        val searchQuery = searchRequest?.takeIf { it.isNotEmpty() }
+        return createPagerFlow {
+            filesDao.listStarred(
+                query = searchQuery,
+                sortingItemType = ItemType.Folder.ordinal,
+                sortingSecondType = sortingSecondType.value
+            ).also { pagingSource = it }
+        }
+    }
+
+    override fun provideStarred(): Flow<PagingData<Item>> {
+        return emptyFlow()
+    }
+
+    override fun setFolderId(id: Long) {
+        folderIdState.update { id }
+    }
+
+    override fun invalidate() {
+        pagingSource?.invalidate()
+    }
+
+}
