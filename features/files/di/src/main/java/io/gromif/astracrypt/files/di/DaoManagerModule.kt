a61bd8d39a81d3a1dda55f965bb57360451f2aa6
gromif <alexander.kobrys@proton.me>
2025-05-27 10:38:51 +0300

    refactor(data): extract reusable DAO caching logic

---
+++ features/files/di/src/main/java/io/gromif/astracrypt/files/di/DaoManagerModule.kt
@@ -0,0 +1,29 @@
+package io.gromif.astracrypt.files.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import io.gromif.astracrypt.files.data.db.DaoManager
+import io.gromif.astracrypt.files.data.db.FilesDao
+import io.gromif.astracrypt.files.data.db.FilesDaoAeadAdapter
+import io.gromif.astracrypt.files.data.util.AeadHandler
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal object DaoManagerModule {
+
+    @Singleton
+    @Provides
+    fun provideDaoManager(
+        filesDao: FilesDao,
+        aeadHandler: AeadHandler,
+    ) = DaoManager(
+        filesDaoAeadAdapterFactory = FilesDaoAeadAdapter.Factory(
+            filesDao = filesDao,
+            aeadHandler = aeadHandler
+        )
+    )
+
+}
