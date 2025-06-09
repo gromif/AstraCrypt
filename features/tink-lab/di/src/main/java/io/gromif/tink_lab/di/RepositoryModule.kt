704fb4d9730eb3c5b9c30fdf10b0e0ffa1b33d37
gromif <alexander.kobrys@proton.me>
2025-06-09 20:51:02 +0300

    refactor(domain): remove obsolete code from `Repository`

--- features/tink-lab/di/src/main/java/io/gromif/tink_lab/di/RepositoryModule.kt
+++ features/tink-lab/di/src/main/java/io/gromif/tink_lab/di/RepositoryModule.kt
@@ -5,10 +5,7 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
 import dagger.hilt.android.scopes.ViewModelScoped
-import io.gromif.crypto.tink.core.encoders.Base64Encoder
-import io.gromif.crypto.tink.keyset.parser.KeysetParser
 import io.gromif.tink_lab.data.repository.RepositoryImpl
-import io.gromif.tink_lab.data.util.TextAeadUtil
 import io.gromif.tink_lab.domain.model.Repository
 import io.gromif.tink_lab.domain.util.KeyGenerator
 import io.gromif.tink_lab.domain.util.KeyReader
@@ -21,19 +18,13 @@ internal object RepositoryModule {
     @ViewModelScoped
     @Provides
     fun provideRepository(
-        textAeadUtil: TextAeadUtil,
-        keysetParser: KeysetParser,
         keyGenerator: KeyGenerator,
         keyWriter: KeyWriter,
         keyReader: KeyReader,
-        base64Encoder: Base64Encoder,
     ): Repository = RepositoryImpl(
-        textAeadUtil = textAeadUtil,
-        keysetParser = keysetParser,
         keyGenerator = keyGenerator,
         keyWriter = keyWriter,
         keyReader = keyReader,
-        base64Encoder = base64Encoder
     )
 
 }
