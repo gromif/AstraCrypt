8192ceb7d0955d91861d661fd741fdf5dc120661
gromif <alexander.kobrys@proton.me>
2025-06-11 12:26:13 +0300

    chore(presentation): create `presentation:recent` module

---
+++ features/files/presentation/recent/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    alias(libs.plugins.astracrypt.android.library)
+    alias(libs.plugins.astracrypt.android.library.compose)
+    alias(libs.plugins.astracrypt.android.hilt)
+    alias(libs.plugins.astracrypt.android.hilt.compose)
+    alias(libs.plugins.astracrypt.android.coil)
+
+    alias(libs.plugins.astracrypt.kotlin.coroutines)
+    alias(libs.plugins.astracrypt.test.unit)
+}
+
+android {
+    namespace = "io.gromif.astracrypt.files.recent"
+}
+
+dependencies {
+    implementation(projects.features.files.domain)
+    implementation(projects.features.files.di)
+    implementation(projects.features.files.presentation.shared)
+}
