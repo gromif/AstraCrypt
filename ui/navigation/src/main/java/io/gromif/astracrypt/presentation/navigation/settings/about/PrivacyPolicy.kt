3903551b79cb1570ee8e46a0b3dce6cca8d6e7b0
gromif <alexander.kobrys@proton.me>
2025-06-28 13:11:33 +0300

    fix(navigation): make `PrivacyPolicy` screen scrollable

--- ui/navigation/src/main/java/io/gromif/astracrypt/presentation/navigation/settings/about/PrivacyPolicy.kt
+++ ui/navigation/src/main/java/io/gromif/astracrypt/presentation/navigation/settings/about/PrivacyPolicy.kt
@@ -1,6 +1,10 @@
 package io.gromif.astracrypt.presentation.navigation.settings.about
 
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
@@ -10,6 +14,7 @@ import io.gromif.astracrypt.presentation.navigation.shared.LocalHostEvents
 import io.gromif.astracrypt.presentation.navigation.shared.UiStateHandler
 import io.gromif.astracrypt.resources.R
 import io.gromif.astracrypt.settings.privacy.PrivacyPolicyScreen
+import io.gromif.ui.compose.core.theme.spaces
 import io.gromif.ui.compose.core.wrappers.TextWrap
 
 private val DefaultUiState = UiState(
@@ -22,5 +27,9 @@ internal fun NavGraphBuilder.privacyPolicy() = composable<Route.AboutGraph.Priva
     val hostEvents = LocalHostEvents.current
     UiStateHandler { hostEvents.setUiState(DefaultUiState) }
 
-    PrivacyPolicyScreen(modifier = Modifier.fillMaxSize())
+    PrivacyPolicyScreen(modifier = Modifier
+        .fillMaxSize()
+        .verticalScroll(rememberScrollState())
+        .padding(MaterialTheme.spaces.spaceMedium)
+    )
 }
