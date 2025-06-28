5279cc2cbfd1b7a26d6087676f19c632070c0811
Alex <47805607+gromif@users.noreply.github.com>
2025-06-28 11:26:38 +0300

    refactor(policy): parse policy via `AnnotatedString` (#70)
    * refactor(policy): remove unnecessary elements
    
    * refactor(policy): improve page readability
    
    * feat(policy): parse policy via `AnnotatedString`
    
    * refactor(detekt): apply code smell auto-correction
    
    * refactor(detekt): remove baseline configuration

--- features/about/privacy/src/main/java/io/gromif/astracrypt/settings/privacy/Screen.kt
+++ features/about/privacy/src/main/java/io/gromif/astracrypt/settings/privacy/Screen.kt
@@ -1,17 +1,30 @@
 package io.gromif.astracrypt.settings.privacy
 
-import android.webkit.WebView
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.viewinterop.AndroidView
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextLinkStyles
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.fromHtml
 
 @Composable
 internal fun Screen(
-    modifier: Modifier = Modifier, privacyPolicyHtml: String
+    modifier: Modifier = Modifier,
+    privacyPolicyHtml: String
 ) = Column(modifier = modifier) {
-    if (privacyPolicyHtml.isNotEmpty()) AndroidView(
-        factory = { context -> WebView(context) },
-        update = { webView -> webView.loadData(privacyPolicyHtml, "text/html", "utf-8") }
+    if (privacyPolicyHtml.isNotEmpty()) return@Column
+    val parsedHtml = AnnotatedString.fromHtml(
+        htmlString = privacyPolicyHtml,
+        linkStyles = TextLinkStyles(
+            style = SpanStyle(
+                color = MaterialTheme.colorScheme.primary,
+                fontWeight = FontWeight.Bold
+            )
+        ),
     )
+    Text(parsedHtml)
 }
