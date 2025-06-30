69e49da5bd52cea5d44ca762298c70db5e4e8558
gromif <alexander.kobrys@proton.me>
2025-06-30 12:58:29 +0300

    refactor(about): introduce `Link` model

---
+++ features/about/src/main/java/io/gromif/astracrypt/settings/about/model/Link.kt
@@ -0,0 +1,22 @@
+package io.gromif.astracrypt.settings.about.model
+
+import androidx.compose.runtime.Immutable
+
+@Immutable
+sealed class Link {
+
+    data class Default(
+        val name: String,
+        val description: String? = null,
+        val link: String,
+    ): Link()
+
+    data object PrivacyPolicy: Link()
+
+    data class Email(
+        val name: String,
+        val description: String? = null,
+        val email: String,
+        val emailSubject: String? = null,
+    ): Link()
+}
