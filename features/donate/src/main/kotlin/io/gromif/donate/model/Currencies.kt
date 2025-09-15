16460198133d299df6358f62414459448e4b38d9
Alex <47805607+gromif@users.noreply.github.com>
2025-09-15 12:31:57 +0300

    chore(donate): update crypto addresses (#84)

--- features/donate/src/main/kotlin/io/gromif/donate/model/Currencies.kt
+++ features/donate/src/main/kotlin/io/gromif/donate/model/Currencies.kt
@@ -11,27 +11,27 @@ internal object Currencies {
     val default = listOf(
         Currency(
             name = "Bitcoin (BTC)",
-            address = "bc1pdsudp0ydnlej4s7ekuh0g3vw6npheqv3sq5jk7tncrc22zekulgqyfeqgs",
+            address = "bc1qfp4qxe6zp69s6fnuyyam7n9vvu92467n2xtpc3",
             iconResId = R.drawable.currency_btc
         ),
         Currency(
             name = "Ethereum (ETH)",
-            address = "0x6AB42792BA41b88983d7d7384B1162210341AC46",
+            address = "0x322b8c1C305b3Ec997B90CBe8EF8402ec37a03d3",
             iconResId = R.drawable.currency_eth
         ),
         Currency(
             name = "Monero (XMR)",
-            address = "42o8RetYnV7PmdENJKg6SvVqWSUy8QBYxECSbp6xreQ6PQ5EETjXVNS7ssqyRrLjMcVL6FAoUyK7tQfwKMNbb3JdLkT1HLK",
+            address = "47cfoZG7MtqLu9wFsQbb6h7K4wkBZZTnuhZX8VTcByzZTWafDX5CT9KMtrChjoDEwqLXdrRYXD36WMwSff7bHWhYLihLqWM",
             iconResId = R.drawable.currency_xmr
         ),
         Currency(
             name = "Litecoin (LTC)",
-            address = "ltc1qch299ndm87a5ssfusxqrnfpwtc4348fpvhys7y",
+            address = "ltc1qm7mf9v2j7cpxxehc67smk0s7me3cnrrc3vpcwu",
             iconResId = R.drawable.currency_ltc
         ),
         Currency(
             name = "Cardano (ADA)",
-            address = "addr1qyyjl0d6sqmxmzq04vjwvg29h6hcvzavdps465htrnlj5zkt3vrxy6lt068kdvlymj45mn70k0c0hqxg8az6ya7r8xwsz2g35p",
+            address = "addr1q8rd94y99cp0nx77azw7h35zwhegnj4hscvjjxtds0plgp4v4tw9q8larcacjrgp3ykhgsu4vhu4drazrwyh5g0r630sg97vag",
             iconResId = R.drawable.currency_ada
         ),
     )
